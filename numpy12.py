a1e1d616071e97ceb7800bc2f4cef9255512e676
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-30 16:24:22 +0530

    shuffle the sequence in list by random function

--- numpy12.py
+++ numpy12.py
@@ -1,3 +1,19 @@
 import random
 f=random.random() #it gives any random number from 0 to 1
 print(f)
+
+
+#rondom choice(sequence)
+s='String'
+t=(1,2,3)
+d={1:2,3:4,5:6}
+l=[1,2,3,4,5]
+print(random.choice(s))
+
+#shuffle method to shuffle the sequence (list)
+print(l)
+random.shuffle(l)
+
+l11=[1,2,3]
+print(l11)
+print(random.choice(l11))
