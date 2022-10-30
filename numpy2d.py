7f414249fb353bb44d6e7ec2706f475ecd91bf45
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-30 14:27:43 +0530

    change in 1d array in numpy

--- numpy2d.py
+++ numpy2d.py
@@ -2,3 +2,7 @@ import numpy as np
 arr=np.array([1,2,3,4])
 print(arr)
 print(type(arr))
+
+arr=np.array([1,9801,3], dtype=np.int32)
+print(arr)
+print(type(arr))
---
+++ numpy3.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr=np.array([1,47363,3], dtype=np.int8)
+print(arr)
