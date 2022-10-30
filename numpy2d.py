2368ca6fb721080a02400b702b6fbdc579098687
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-30 14:37:17 +0530

    NumPy shape, size and data type

--- numpy2d.py
+++ numpy2d.py
@@ -3,6 +3,21 @@ arr=np.array([1,2,3,4])
 print(arr)
 print(type(arr))
 
-arr=np.array([1,9801,3], dtype=np.int32)
+arr=np.array([1,9801,3], dtype=np.int8)
 print(arr)
 print(type(arr))
+
+
+arr[0]
+arr[1]=0
+print(arr)
+
+arr[0]
+arr[2]=0
+print(arr)
+
+darr1=np.array([[1,2,3,4],[5,6,7,8]])
+print(darr1)
+print(darr1.shape)
+print(darr1.size)
+print(darr1.dtype)
