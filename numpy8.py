3a8c081ca773928884184c9026e561b6da909df2
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-30 15:52:28 +0530

    Random numpy

---
+++ numpy8.py
@@ -0,0 +1,24 @@
+import numpy as np
+a=np.arange(10)
+print(a)
+
+a=np.zeros((3,4))
+print(a)
+
+a=np.ones(3)
+a=np.ones((2,3))
+print(a)
+
+a=np.empty([2,3])
+print(a)
+
+a=np.linspace(1,5,8)
+print(a)
+
+
+n=np.arange(9)
+print(n)
+p=np.reshape(p)
+
+res=p.ravel()
+print(res)
---
+++ numpy9.py
@@ -0,0 +1,3 @@
+import random 
+l=[1,2,3,4,5]
+print(random.choice(l))
