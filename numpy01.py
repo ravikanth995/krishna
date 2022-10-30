8d5e4b83f0f4509887788bfa145bca29a9dcc0ab
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-30 17:13:00 +0530

    numpy single and two dimensional array

---
+++ numpy01.py
@@ -0,0 +1,15 @@
+#1D array in NumPy
+import numpy as np
+a=np.array([1,2,3,4])
+print(a)
+
+
+#2D array in NumPy
+a=np.array([1,2022,4], dtype=np.int8)
+print(a)
+print(type(a))
+
+a[0]=0
+a[1]=1
+a[2]=2
+print(a)
