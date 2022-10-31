ad252ebf26e3d76c7f3700f52aea4f5bb9c689cc
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-31 22:51:09 +0530

    numpy random basic

--- numpy08.py
+++ numpy08.py
@@ -1,9 +1,10 @@
 #random choice(sequence)
+import numpy as np
 import random
 # s='ravi'
 # t=(1,2,3)
 # d={1,2,3,}
-# l=[1,2,3,4,5,6]
+l=[1,2,3,4,5,6]
 # # print(random.choice(t))
 # it prints any element of the data type that is prescribed in the print(random.choice(here))
 
@@ -15,7 +16,14 @@ import random
 # l1=[1,2,3]
 # print(random.choice(l1))
 
-np.random.random(2)
-np.random.rand(3,2)
-np.random.randint(2, size=4)
+a=np.random.random(2)
+a=np.random.rand(3,2)
+a=np.random.randint(2, size=4)
+print(a)
+
+
+
+arr=np.arange(9)
+np.random.shuffle(arr)
+print(arr)
 
---
+++ numpy09.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+import random
+
+np.random.random(2)
+np.random.rand(3,2)
+np.random.randint(2, size=4)
+
+arr=np.arange(9)
+print(arr)
+np.random.shuffle(arr)
+print(arr)
