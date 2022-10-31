4cd834d86f1f7f936cadc59f9ff70e37dbfaa1df
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-31 22:33:12 +0530

    Random basic

---
+++ numpy07.py
@@ -0,0 +1,17 @@
+import random
+# l=[1,2,3,4]
+# print(random.choice(l))
+
+# import random 
+# random.seed(5)
+# print(random.random())
+# print(random.random())
+
+#random int(start, end) used to generate random integers only in between the range
+# import random
+
+r1=random.randint(-5,15)
+print(r1)
+
+f=random.random()
+print(f)
