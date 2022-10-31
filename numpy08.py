f5be068b4fdc1d7f85705b33f1cdcf552a10c837
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-31 22:44:27 +0530

    random basic1

---
+++ numpy08.py
@@ -0,0 +1,21 @@
+#random choice(sequence)
+import random
+# s='ravi'
+# t=(1,2,3)
+# d={1,2,3,}
+# l=[1,2,3,4,5,6]
+# # print(random.choice(t))
+# it prints any element of the data type that is prescribed in the print(random.choice(here))
+
+
+#shuffle method to shhuffle the sequence
+# random.shuffle(l)
+# print(l)
+
+# l1=[1,2,3]
+# print(random.choice(l1))
+
+np.random.random(2)
+np.random.rand(3,2)
+np.random.randint(2, size=4)
+
