68388e7c20680ed0aaf31b7befdacc1e56986fc5
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-31 22:25:08 +0530

    NumPy basics

--- numpy06.py
+++ numpy06.py
@@ -7,3 +7,17 @@ print(arr1)
 
 arr2=np.ones((2,3))
 print(arr2)
+
+arr2=np.empty([2,3])
+print(arr2)
+
+arr4=np.linspace(1,5,12)
+print(arr4)
+
+n=np.arange(9)
+print(n)
+p=n.reshape(3,3)
+print(p)
+
+res=p.ravel()
+print(res)
