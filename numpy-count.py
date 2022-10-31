f8746a560f773a1a2057442756a7f9daf0d9f11b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-10-31 23:09:08 +0530

    numpy count

---
+++ numpy-count.py
@@ -0,0 +1,9 @@
+import numpy as np
+# arr=['AAtukuhsfr', 'kbcksnv', 'JHGjgcAAusrvrv']
+# print(np.char.count(arr, 'AA'))
+
+
+a=[1,2,3,1,2]
+print(np.unique(a))
+print(np.unique(a, return_counts=True))
+
---
+++ numpy-sort.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#sorting
+# a=np.array([(1,3),(2,4)])
+# print(a)
+# print(np.sort(a))
+# print(np.sort(a, axis=None))
+
+# a=np.array([1,3,2]) #it gives numbers in a ascending index wise not number wise
+# a=np.argsort(a)
+# print(a)
+
+# a1=np.sort_complex()
+# print(a1)
+
+b=np.arange(6)
+print(b)
+a=b[2]
+print(np.argmax(a))
