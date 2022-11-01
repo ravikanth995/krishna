6fcf8d215407f5f5c8a934e486a0654bcaad2603
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-01 19:38:42 +0530

    Arguments

--- args6.py
+++ args6.py
@@ -16,11 +16,32 @@
 # item=['ek','do','teen']
 # display(name, *item)        
 
-def osho(**kwargs):
-    for key, value in kwargs.items():
-        print('%s==%s'%(key,value))
+# def osho(**kwargs):
+#     for key, value in kwargs.items():
+#         print('%s==%s'%(key,value))
 
-osho(first='hello',mid='namaste', last='bye')        
+# osho(first='hello',mid='namaste', last='bye')    
 
+
+# def myfun(*args, **kwargs):
+#     :
+# print('*args:',args)
+# print('kwargs',kwargs)
+
+# myfun('HEllo','python','world', first='hello', mid='python', last='world')
+    
+
+def display(name, *args, **kwargs):
+    print(name)
+    for i in args:
+        print(i)
+        for k, v in kwargs.items():
+            print(f'{k} in a {v}')
+
+name='Roopa'
+item=['1','2','3']
+d={'ravi':'kanth','hari':'singh'} 
+display(name, *item, **d)               
+    
 
 
