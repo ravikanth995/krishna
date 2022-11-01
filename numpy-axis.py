9dd4e16ddccd1573f0038e6ce2668f91b775d199
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-01 12:23:09 +0530

    Axis numpy

---
+++ numpy-axis.py
@@ -0,0 +1,15 @@
+import numpy as np
+#axis
+x=[[1,2,3],[4,5,6]]
+ar=np.array(x)
+# print(ar)
+# ar.shape
+# print(ar)
+
+# ar=ar.sum(axis=0)
+# ar=ar.sum(axis=1)
+# print(ar)
+
+#transpose
+ar=ar.T
+print(ar)
--- numpy-sorting.py
+++ numpy-sorting.py
@@ -1,16 +1,22 @@
 import numpy as np
-a=np.array([(1,4),(1,3)])
-print(np.sort(a))
-print(np.sort(a, axis=None))
+# a=np.array([(1,4),(1,3)])
+# print(np.sort(a))
+# print(np.sort(a, axis=None))
 
 
-a=np.array([1,3,2])
-print(a)
-np.argsort(a)
+# a=np.array([1,3,2])
+# print(a)
+# np.argsort(a)
 
-np.sort_complex([5+15j,3,6,2,1])
+# np.sort_complex([5+15j,3,6,2,1])
 
-b=np.arange(6)
-print(b)
-# a=b[2]
-print(np.argmax(a))
+# b=np.arange(6)
+# print(b)
+# # a=b[2]
+# print(np.argmax(a))
+
+
+
+#Count
+arr=['kjddvstt',['bcbctt'],['hfhfsesef']]
+print(np.char.count(arr,'tt'))
