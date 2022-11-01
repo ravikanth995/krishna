28fd5bc4bb139e5b54642849e24f3a8e2e317eb1
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-01 12:07:32 +0530

    Numpy sorting

---
+++ numpy-sorting.py
@@ -0,0 +1,16 @@
+import numpy as np
+a=np.array([(1,4),(1,3)])
+print(np.sort(a))
+print(np.sort(a, axis=None))
+
+
+a=np.array([1,3,2])
+print(a)
+np.argsort(a)
+
+np.sort_complex([5+15j,3,6,2,1])
+
+b=np.arange(6)
+print(b)
+# a=b[2]
+print(np.argmax(a))
