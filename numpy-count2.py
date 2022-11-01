5e2098abb20f4627c22334bc405af2fb7e5684b8
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-01 11:56:21 +0530

    numpy random

---
+++ numpy-count2.py
@@ -0,0 +1,13 @@
+import numpy as np
+# a=[1,2,3,1,2]
+# print(np.unique(a))
+# print(np.unique(a, return_count=True))
+
+np.random.random(2)
+np.random.rand(3,2)
+np.random.randint(2, size=5)
+# print(a)
+7
+arr=np.arange(10)
+np.random.shuffle(arr)
+print(arr)
