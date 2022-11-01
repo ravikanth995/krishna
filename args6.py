f7bdf0d3f1f1359f86efdc46435babaf2ad1ba03
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-01 17:54:59 +0530

    KEy word arguments

--- args6.py
+++ args6.py
@@ -7,11 +7,20 @@
 #     print(*item)
 # print('phone','cool','margin')    
 
-def display(name, *args):
-    print(name)
-    for i in args:
-        print(i)
+# def display(name, *args):
+#     print(name)
+#     for i in args:
+#         print(i)
 
-name='Gun'
-item=['ek','do','teen']
-display(name, *item)        
+# name='Gun'
+# item=['ek','do','teen']
+# display(name, *item)        
+
+def osho(**kwargs):
+    for key, value in kwargs.items():
+        print('%s==%s'%(key,value))
+
+osho(first='hello',mid='namaste', last='bye')        
+
+
+
