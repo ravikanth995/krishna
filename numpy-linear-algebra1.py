6c3c2e983ec7acfe286392e8cdcd37681cca3a57
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-02 22:21:06 +0530

    NUmPy algebra

---
+++ numpy-linear-algebra1.py
@@ -0,0 +1,44 @@
+import numpy as np
+# import math
+# v=np.array([[1],[2],[3]])
+# print(v)
+# print(v.T)
+
+# v1=np.array([1,2,3])
+# v2=np.array([4,5,6])
+# np.dot(v1,v2)
+# print(v1+v2)
+
+# m=np.array([[1,2,3],[4,5,6]])
+# print(m)
+
+# ten=np.array([[[1,2,3,4],[5,6,7,8],[9,8,7,6]]])
+# ten.shape
+# print(ten)
+
+# print(math.pi)
+# in_array=(0,math.pi/2, np.pi/3,np.pi)
+# print('\n cosin value :\n', in_array)
+# print('input array is:', in_array)
+# cos_value=np.cos(in_array)
+# print('\n cosin value is:\n', cos_value)
+
+#numpy strings
+# a1=np.array(('My Name Is:'))
+# a2=np.array(('Ravikanth'))
+# print(np.char.add(a1,a2))
+
+# #Multiplication of char
+# a1=np.array((' I Love You'))
+# print(np.char.multiply(a1,4))
+
+#Capital
+# a1=np.array(['i','am','ravi'])
+# print(np.char.capitalize(a1))
+
+#format
+# print(np.char.center('Python',18,fillchar='*'))
+
+arr=['hmchmmjh','gddhudmm','hggmmkgfemm']
+print(np.char.count(arr,'mm'))
+
