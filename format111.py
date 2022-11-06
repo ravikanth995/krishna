29fa675e97f56198e76df3028d9122a7194b5caa
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-06 22:18:25 +0530

    formating

---
+++ format111.py
@@ -0,0 +1,6 @@
+name='ravikanth'
+#print( 'Hello %s Welcome to hi'%(name))
+num=1537199
+#print('hello %s welcome to Hi your number is %d'%(name,num))
+fee=19000
+print('Hello %s welcome to Hi\n''your number is %d \n''your amount id %.2f'%(name,num,fee))
