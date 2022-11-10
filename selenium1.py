bab76e3862139d4966136fe849fa752beb1ffa64
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-10 22:56:21 +0530

    Selenium solved

---
+++ selenium1.py
@@ -0,0 +1,13 @@
+import sys
+m=sys.argv
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+app=QApplication([sys.argv[0]])
+window=QWidget()
+window.setWindowTitle("PyQt ap")
+window.setGeometry(100,100,300,90)
+
+helloMsg=QLabel("<h1> Hello, World!<hq>",parent=window)
+helloMsg.move(60,15)
+
+window.show()
+sys.exit(app.exec())
