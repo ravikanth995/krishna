dbad1964678d9bd3d5f4157aea20783f6dc2b0e6
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-14 11:36:12 +0530

    class function1

---
+++ class_function1.py
@@ -0,0 +1,10 @@
+class student:
+    std_name='Ravikanth\t'
+    std_class=10
+    std_id=1537199
+    std_bood='A+'
+    std_mother="KAsubai\t"
+    std_caste="gormati"
+
+s1=student()
+print(s1.std_name, s1.std_id, s1.std_bood, s1.std_class, s1.std_mother, s1.std_caste)
