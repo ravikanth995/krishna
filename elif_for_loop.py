4ed71458f3a6c3b2db26b5bb624eda2c8f321480
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-14 10:54:46 +0530

    elif input method

---
+++ elif_for_loop.py
@@ -0,0 +1,7 @@
+num=int(input("Enter The Number:"))
+if(num>0):
+    print("Entered Number is Positive")
+elif(num<0):
+    print("Entered Nunber is Negative") 
+elif(num==0):
+    print("Entered Number is Zero")       
