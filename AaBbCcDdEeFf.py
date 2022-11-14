1349f37945d040565f44cca890361b4d5b1a836c
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-14 21:44:14 +0530

    Large Numbers into commas

--- AaBbCcDdEeFf.py
+++ AaBbCcDdEeFf.py
@@ -2,8 +2,7 @@ string1=input("Enter First String")
 string2=input("Enter Second String")
 if(len(string1)==len(string2)):
     print("Strings are of same length")
-    result=""
-    for i in range(len(string1)):
+for i in range(len(string1)):
         result=result+(string2[i]+string1[i])
         print(result)
 else:
