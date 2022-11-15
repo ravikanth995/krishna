6e5927ffb95bac81807d551e6e4d38fcf24b42b9
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-15 21:59:06 +0530

    if-statement

---
+++ name-age-of-candidate.py
@@ -0,0 +1,9 @@
+name=input("Enter Your Name")
+age=int(input("Hi%s, Enter Your Age:"%name))
+
+if (age<24):
+    print("Congratulations ! %s"%name+'..')
+    print("You are eligible for Military Services")
+if (age>24):
+    print("Sorry %s "%name,"..")
+    print("You are not eligible for Military services")
