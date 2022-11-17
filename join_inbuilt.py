e9bf60292db2ac854642e15de0f02ca75c9a45f8
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-17 22:36:24 +0530

    Join inbuilt functoin

---
+++ join_inbuilt.py
@@ -0,0 +1,9 @@
+# list=['python\t','programming\t','language']
+# separator=""
+# str=separator.join(list)
+# print(str)
+
+list=['Python','Progaraming','Langauge']
+separator=""
+str=separator.join(list)
+print(str)
