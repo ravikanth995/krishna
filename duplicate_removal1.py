bb334219c9dc8ae32f5d5d50b27d74cfdb4189e4
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-17 10:14:00 +0530

    Fibanacci 0 to 8

---
+++ duplicate_removal1.py
@@ -0,0 +1,6 @@
+num=list(int(map(input(int, input("Enter the elements into list with duplication").split(',')))))
+s=[]
+for i in num:
+    if i not in s:
+        s.append()
+print(s)        
---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+num=8
+x=0
+y=1
+print(0)
+for i in range(0,num):
+    z=x+y
+    x=y
+    y=z
+    print(z)
