fb6342d4e36272076721201520ebd2b2ba6b450d
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-19 23:09:46 +0530

    Inbuilt functions

--- built_in_function.py
+++ built_in_function.py
@@ -81,5 +81,93 @@
 # print(l)
 
 
-l=round(9.6267)
-print(l)
+# l=round(9.6267)
+# print(l)
+
+# #Reversed inbuilt function
+# str='Python'
+# print(list(reversed(str)))
+
+
+
+# str='Ravikanth'
+# print(list(reversed(str)))
+
+
+# str="Ravikanth_chavan"
+# print(list(reversed(str)))
+
+
+# str='Ravi'
+# print(list(reversed(str)))
+
+
+#Map Inbuilt Functions
+#map(function, iterable)
+
+
+# def add(num):
+#     return num+num
+# res=map(add,[3,2,1])
+# print(list(res))
+
+
+
+
+# def add(num):
+#     return num+num
+# res=map(add,[5,4,3,2,1])
+# print(list(res))    
+
+
+# def add(num):
+#     return num+num
+# res=map(add,(3,2,1))
+# print(tuple(res)) 
+# 
+# 
+# 
+# def add(num):
+#     return num+num
+# res=map(add,[4,3,2,1])
+# print(list(res))       
+
+
+#Split This method is used to split a string into list of elements
+
+# #string.split("separator")
+# str='Python is programming language'
+# list=str.split("is")
+# print(list)
+
+
+# string="python is not a readable code"
+# list=string.split("not")
+# print(list)
+
+# str='Ravikanth is selected for this job'
+# list=str.split("is")
+# print(list)
+
+# str="Ravikanth is hired for this job"
+# list=str.split("is")
+# print(list)
+
+# str="lucky is not lucky"
+# list=str.split("not")
+# print(list)
+
+# #Split
+# str="lucky is not enough lucky"
+# list=str.split("not")
+# print(list)
+
+
+#join Inbuilt Function 
+#This method is used to take all items from an iterable and join them into single string
+#string.join(iterable)
+
+# list=['Python','Programming','Language']
+# separator="\t"
+# string=separator.join(list)
+# print(string)
