b745c83f62e757c7eb8b417c8a5e5140d7ccbf77
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 13:33:03 +0530

    Center os string

--- string_method.py
+++ string_method.py
@@ -7,6 +7,22 @@
 # print(l1)
 
 
-str='ravi'
-l1=str.capitalize()
+# str='ravi'
+# l1=str.capitalize()
+# print(l1)
+
+#Center()
+#this method has the two arguemnts. one is width of the string and the other is the filling character
+#string_name.center(width, filling_char)
+
+# str='HELLO'
+# l1=str.center(15,"#")
+# print(l1)
+
+# str='HI'
+# l1=str.center(8,"#")
+# print(l1)
+
+str="HI"
+l1=str.center(6,"*")
 print(l1)
