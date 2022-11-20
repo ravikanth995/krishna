5d9671c46101cc407c6e540deccd648b09c1940b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 12:11:44 +0530

    List Functions

---
+++ list_methods.py
@@ -0,0 +1,11 @@
+#List Methods
+#Returns the count of the required value in the list
+#list_name.count(value)
+
+# list=[1,2,3,3,3,5,6,4,5,3]
+# l2=list.count(3)
+# print(l2)
+
+list=[2,3,3,4,4,5,5,5,5,5,5,5,6,7,8,9]
+l1=list.count(5)
+print(l1)
