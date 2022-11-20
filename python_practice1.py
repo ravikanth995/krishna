23a426146fb394ff2810a3825173e5eb080bab3d
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 16:29:41 +0530

    Write a program that asks the user to enter three numbers(use three separate input statements). Create varaibles called total and average of the three numbers and print out the values of total and avearge

---
+++ python_practice1.py
@@ -0,0 +1,32 @@
+# num=int(input("Enter First number"))
+# num1=int(input("Enter Second number"))
+# num2=int(input("Enter Third number"))
+# sum=num+num1+num2
+# average=sum/3
+# print("The Sum of all Num is:", sum)
+# print("Average of all three num is:", average)
+
+# num1=int(input("Enter First Number"))
+# num2=int(input("Enter Second Number"))
+# num3=int(input("Enter Third Number"))
+# sum=num1+num2+num3
+# avg=sum/3
+# print("The Sum of all Three Number is:", sum)
+# print("The Average of all three numbers is: ", avg)
+
+# num1=int(input("Enter First Number"))
+# num2=int(input("Enter Second Number"))
+# num3=int(input("Enter Third Number"))
+# sum=num1+num2+num3
+# avg=sum/3
+# print("The Sum of all number is:", sum)
+# print("Average of all three numbers is:", avg)
+
+
+num1=float(input("Enter First Number"))
+num2=int(input("Enter Second Number"))
+num3=int(input("Enter Third Number"))
+sum=num1+num2+num3
+avg=sum/3
+print("Average of all three values is:", sum)
+print("Average of all three value is:", avg)
