16d5d35acec40a04230cfb4f176ca38515f16794
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 13:56:02 +0530

    Count of string

--- string_method.py
+++ string_method.py
@@ -23,6 +23,18 @@
 # l1=str.center(8,"#")
 # print(l1)
 
-str="HI"
-l1=str.center(6,"*")
+# str="HI"
+# l1=str.center(6,"*")
+# print(l1)
+
+#Count
+#it returns the number of times the value is repeated in the string. It consists of three arguments, one is the string and other two are begining and end of strig which denoted starting and ending indices.
+#string_name.count(string,begining,end)
+
+# str='welcome to python programming'
+# l1=str.count('w',0,len(str)) #w is printed is only one time the string, so output will be 1 and e is two times, so output will be 2
+# print(l1)
+
+str='ravi kanth'
+l1=str.count('a',0,len(str))
 print(l1)
