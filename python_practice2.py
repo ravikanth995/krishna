603761891ad9252414d322a0bc0e3a2dbab52767
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 17:00:11 +0530

    Write a program that asks the user for their name and how many times to print it. The program should print out the users name and specify the number of times

---
+++ python_practice2.py
@@ -0,0 +1,51 @@
+# name=input("Enter your Name:")
+# n=int(input("Enter How Many Times You Want To print"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter the name")
+# n=int(input("Enter How Many times you want to print"))
+# for i in range(n):
+#     print(name)
+
+
+# name=input("enter a number")
+# n=int(input("How many times you want to print"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter The Name:")
+# n=int(input("Enter The times of the name to be printed"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter The Content:")
+# n=int(input("How many times you want to print"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter The Name:")
+# n=int(input("Enter how many times you want to prinnt"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter Name:")
+# n=int(input("Enter How many times you want to print:"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter content:")
+# n=int(input("Enter Number times you want to print:"))
+# for i in range(n):
+#     print(name)
+
+# name=input("Enter the name:")
+# n=int(input("Enter How Many Times:"))
+# for i in range(n):
+#     print(name)
+
+
+name=input("Enter name:")
+n=int(input("Enter how many times:"))
+for i in range(n):
+    print(name)
