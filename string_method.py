50ae8e3431787b4e06ff0fe2fa7f1614dbc084c2
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 13:23:51 +0530

    Capitalizes the first string later

---
+++ string_method.py
@@ -0,0 +1,12 @@
+#Capitalize
+#first letter of the string will be converted into capital
+#string_name.capitalize()
+
+# str='ravi is kanth its ravikanth'
+# l1=str.capitalize()
+# print(l1)
+
+
+str='ravi'
+l1=str.capitalize()
+print(l1)
