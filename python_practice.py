19a67704f372010fe6519a0b5bec5a6e522892c9
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-20 15:54:11 +0530

    Write a program that asks the user for a weight in klograms and converts it to pounds. there are 2.2 pounds in a kilograms

---
+++ python_practice.py
@@ -0,0 +1,23 @@
+# kilogram=float(input("Enter Weight in Kilograms"))
+# pounds=2.2*kilogram
+# print("Weight in Pounds", pounds)
+
+# Kilograms=float(input("Enter Weight in Kilograms"))
+# pounds=2.2*Kilograms
+# print("Weight in pounds", pounds)
+
+# Kilograms=float(input("Weight in KIlograms:"))
+# pounds=2.2*Kilograms
+# print("Weight in pounds",pounds)
+
+# Kg=float(input("Enter Weight in Kg:"))
+# pounds=2.2*Kg
+# print("Weight in Pounds is:", pounds)
+
+# Kg=float(input("Enter Weight in Kilograms"))
+# pounds=2.2*Kg
+# print("Weight in Pounds:", pounds)
+
+Kg=float(input("Enter Weight in Kilograms"))
+pounds=2.2*Kg
+print("Weight in Pounds:", pounds)
