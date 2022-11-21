bce8a2a4749d863b54b629daebec9c30970a09ea
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-21 21:33:46 +0530

    Write a program that asks the user for two numbers and prints close if the numbers are within 0.001 of each other and Not close otherwise

---
+++ python_practice5.py
@@ -0,0 +1,98 @@
+# from decimal import *
+# num1=Decimal(input("Enter Number 1:"))
+# num2=Decimal(input("Enter Number 2:"))
+# diff=abs(num1-num2)
+# if(diff<=0.001):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter Num 1:"))
+# num2=Decimal(input("Enter Num 2:"))
+# diff=abs(num1-num2)
+# if(diff<=0.001):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter num 1:"))
+# num2=Decimal(input("Enter Num 2:"))
+# diff=abs(num1-num2)
+# if(diff<=diff):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter Num 1:"))
+# num2=Decimal(input("Enter Num 2:"))
+# diff=abs(num1-num2)
+# if(diff<=0.001):
+#     print("Close")
+# else:
+#     print("Not Close")
+
+# from decimal import *
+# num1=Decimal(input("Enter Num 1:"))
+# num2=Decimal(input("Enter Num 2:"))
+# diff=abs(num1-num2)
+# if(diff<=0.012):
+#     print("Close")
+# else:
+#     print("Not Close")
+
+# from decimal import *
+# num1=Decimal(input("Enter First Value :"))
+# num2=Decimal(input("Enter Second Value:"))
+# diff=abs(num1-num2)
+# if(diff<=0.002):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter First Value:"))
+# num2=Decimal(input("Enter Second Value:"))
+# diff=abs(num1-num2)
+# if(diff<=0.1):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter First Value"))
+# num2=Decimal(input("Enter Second Value"))
+# diff=abs(num1-num2)
+# if(diff<=12):
+#     print("Close")
+# else:
+#     print("Not Close")
+
+# from decimal import *
+# num1=Decimal(input("Enter First Value:"))
+# num2=Decimal(input("Enter Second Value:"))
+# diff=abs(num1-num2)
+# if(diff<=56):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+# from decimal import *
+# num1=Decimal(input("Enter First Value:"))
+# num2=Decimal(input("Enter Second Value:"))
+# diff=abs(num1-num2)
+# if(diff<=0.002):
+#     print("Close")
+# else:
+#     print("Not Close")    
+
+from decimal import *
+num1=Decimal(input("Enter First Value:"))
+num2=Decimal(input("Enter Second Value:"))
+diff=abs(num1-num2)
+if(diff<=0.01893):
+    print("Close")
+else:
+    print("Not Close")    
