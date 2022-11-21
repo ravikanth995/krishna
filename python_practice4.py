cd9015c05d8a93a69bc86d52dc727324a3ddb363
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-21 19:43:44 +0530

    Generate a random number between 1 to 10. ask the user to guess the number and print a messege based on wether they get it right or not

---
+++ python_practice4.py
@@ -0,0 +1,84 @@
+# import random
+# n=random.randint(1,10)
+# username=int(input("Enter a number between 1 to 10(inclusive):"))
+# if(n==username):
+#     print("Your Guess is Right")
+# else:
+#     print("Your Guess is Wrong") 
+# print("Random Number:",n) 
+
+
+# import random
+# n=random.randint(1,3)
+# usernumber=int(input("Enter a number between 1 to 3(inclusive):"))
+# if(n==usernumber):
+#     print("Your Guess is Right")
+# else:
+#     print("Your Guess is Wrong") 
+# print("Random Number is:",n)       
+
+
+# import random
+# n=random.randint(1,10)
+# usernumber=int(input("Enter a number between 1 to 10(inclusive):"))
+# if(n==usernumber):
+#     print("Your Guess is Right")
+# else:
+#     print("Your Guess is Wrong") 
+# print("Random Number:", n)    
+
+# import random
+# n=random.randint(1,10)
+# usernumber=int(input("Enter a number 1 to 10(inclusive):"))   
+# if(n==usernumber):
+#     print("Your Guess is Right" )
+# else:
+#     print("Your Guess is Wrong") 
+# print("Random Number is:",n)       
+
+# import random
+# n=random.randint(1,10)
+# usernumber=int(input("Enter a Number"))
+# if(n==usernumber):
+#     print("Your Guess is Correct")
+# else:
+#     print("Your Guess is Wrong")
+# print("Your Random Number is",n)        
+
+
+# import random
+# n=random.randint(1,10)
+# usernumber=int(input("Enter a Number"))
+# if(usernumber==n):
+#     print("Your Guess is Right")
+# else:
+#     print("Your Guess is right")
+# print("Random Number is:",n)        
+
+
+# import random
+# n=random.randint(1,10)
+# user_number=int(input("Enter a random number between 1 to 10:"))
+# if(user_number==n):
+#     print("Your Guess is Right")
+# else:
+#     print("Your Guess is Wrong") 
+# print("Random number is:",n)     
+
+# import random
+# n=random.randint(1,10)
+# user_number=int(input("Enter a number"))
+# if(user_number==n):
+#     print("Your guess is right") 
+# else:
+#     print("Your guess is wrong")     
+# print("Random number is:",n)    
+
+import random
+n=random.randint(1,10)
+user_number=int(input("Enter a number"))
+if(user_number==n):
+    print("Your guess is right")
+else:
+    print("Your guess is wrong")    
+print("random number is:",n)    
