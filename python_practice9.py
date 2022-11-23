90a8e79724f62a1546f84a8cf735cb539c7d6dcc
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-23 22:11:16 +0530

    Program for user for a large integer and inserts commas into it according to the standard american convention for commas in large number

---
+++ python_practice9.py
@@ -0,0 +1,44 @@
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Numbers:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter long number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Numbers:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long Number:"))
+# print("{:,}".format(num))
+
+# num=int(input("Enter Long NUmber:"))
+# print("{:,}".format(num))
+
+num=int(input("Enter long number:"))
+print("{:,}".format(num))
