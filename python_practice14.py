64a9de7a4b923ed0245c4236858ee22b9e2ffce2
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-25 21:45:15 +0530

    Write a program that removes any repeated items a list so that each item appears at most once

---
+++ python_practice14.py
@@ -0,0 +1,62 @@
+# List=list(map(int,input("Enter the elements into list duplicates:").split(',')))
+# l=[]
+# for i in List:
+#     if i not in l:
+#         l.append(i)
+# print(l)        
+
+# List=list(map(int,input("Enter elements into list with duplicates:").split(',')))
+# l=[]
+# for i in List:
+#     if i not in l:
+#         l.append(i)
+# print(l)        
+
+# l=list(map(int,input("Enter elements with duplicates:").split(',')))
+# s=[]
+# for i in l:
+#     if i not in s:
+#         s.append(i)
+# print(s)        
+
+# l=list(map(int,input("Enter elements with duplicates:").split(',')))
+# s=[]
+# for i in l:
+#     if i not in s:
+#         s.append(i)
+# print(s)        
+
+# l=list(map(int,input("Enter elements with duplicates:").split(',')))
+# s=[]
+# for i in l:
+#     if i not in s:
+#         l.append(i)
+# print(s)      
+
+# l=list(map(int, input("Enter elements with duplicates:").split(',')))
+# s=[]
+# for i in l:
+#     if i not in s:
+#         s.append(i)
+# print(s)        
+
+# l=list(map(int, input("Enter elements into list with duplicates:").split(',')))
+# f=[]
+# for i in l:
+#     if i not in f:
+#         f.append(i)
+# print(f)        
+
+# l=list(map(int, input("Enter elements into list with duplicates:").split(',')))
+# s=[]
+# for i in l:
+#     if i not in s:
+#         s.append(i)
+# print(s)        
+
+l=list(map(int, input("Enter elements into list with duplicates:").split(',')))
+s=[]
+for i in l:
+    if i not in s:
+        l.append(i)
+print(s)        
