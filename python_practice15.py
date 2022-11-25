e8ea59f19260ec478379bddbf4269f043e2e54a3
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-25 23:08:21 +0530

    Write a function called number_of_factors that takes an integer and returns how mnay factors the number has

---
+++ python_practice15.py
@@ -0,0 +1,150 @@
+# def number_of_factors(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if(n%i==0):
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an integer:"))
+# factors=number_of_factors(num)
+# print("number of factors is:", factors)    
+
+# def number_of_factors(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if(n%i==0):
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_factors(num)
+# print("Factors count is:", factors)
+
+# def number_of_factors(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter a Integer:"))
+# factors=number_of_factors(num)
+# print("Factors count is:", factors)            
+
+# def number_of_factors(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:")) 
+# factors=number_of_factors(num)
+# print("Factors count is:", factors)           
+
+# def number_of_counts(n):
+#     fact_count=0
+#     for i in range(1,n+3):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:")) 
+# factors=number_of_counts(num)  
+# print(" Factors Count is:", factors)     
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an integer:"))
+# factors=number_of_count(num)
+# print("Number of factors:", factors)            
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_count(num)
+# print("Factors of count is:", factors)            
+
+# def number_of_counts(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count 
+# num=int(input("Enter an Integer:"))
+# factors=number_of_counts(num)
+#print("Factors of count:",factors)
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_count(num)
+# print("factors of counts:", factors)            
+
+# def number_of_counts(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter a Integer:"))
+# factors=number_of_counts(num)
+# print("Factors count is:", factors)           
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_count(num)
+# print("Factors counts  is:", factors)
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter a Integer:"))
+# factors=number_of_count(num)
+# print("factor count is:",factors)
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_count(num)
+# print("Factor counts is:", factors)
+
+# def number_of_count(n):
+#     fact_count=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fact_count+=1
+#     return fact_count
+# num=int(input("Enter an Integer:"))
+# factors=number_of_count(num)
+# print("Factor count is:", factors)
+
+def number_of_count(n):
+    fact_count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            fact_count+=1
+    return fact_count
+num=int(input("Enter an Integer:"))
+factors=number_of_count(num)
+print("Factors counts is:", factors)            
+
