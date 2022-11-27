2041156aee522421560a21c55f92bf80e7970f56
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-27 13:12:17 +0530

    Write a function called is_sorted that is given a list and returns True if the list is sorted and False otherwise

---
+++ python_practice18.py
@@ -0,0 +1,121 @@
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if l==x:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter list items:").split()) 
+# print(is_sorted(l))           
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if l==x:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter List Items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if l==x:
+#         return True
+#     else:
+#         return False
+# l=list(input('Enter List Items:').split())
+# print(is_sorted(l))               
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter LIst items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else: 
+#         return False
+# l=list(input("Enter List Items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter List Items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter List Items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True 
+#     else:
+#         return False
+# l=list(input("Enter List items:").split())
+# print(is_sorted(l))     
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()       
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter List integers:").split())
+# print(is_sorted(l))        
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter LIst items:").split())
+# print(is_sorted(l))            
+
+# def is_sorted(l):
+#     x=l[:]
+#     x.sort()
+#     if x==l:
+#         return True
+#     else:
+#         return False
+# l=list(input("Enter List Items:").split())
+# print(is_sorted(l))            
+
+def is_sorted(l):
+    x=l[:]
+    x.sort()
+    if x==l:
+        return True
+    else:
+        return False
+l=list(input("Enter List Items:").split())
+print(is_sorted(l))            
+
+
