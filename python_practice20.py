e86d536d023903984568a11112b592c0ea969a7a
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-27 20:37:22 +0530

    Write a function called merged that takes two already sorted lists of possibly different lengths, and merges them into  a sigle sorted lists

--- python_practice20.py
+++ python_practice20.py
@@ -34,11 +34,164 @@
 # x=merge(a,b)
 # print("List After Sorted:",x)    
 
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("Sorted after:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter Sorted List 1:").split()))
+# b=list(map(int, input("Enter Sorted List 2:").split()))
+# x=merge(a,b)
+# print("Lists After Sorted:",x)
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List sorted:").split()))    
+# b=list(map(int, input("Enter LIst Sorted:").split()))
+# x=merge(a,b)
+# print("List After Sorted:",x)
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("List after Sorted:",x)
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int,input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("List after Sorted:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# x=list(map(int, input("Enter List 1:").split()))
+# y=list(map(int, input("Enter List 2:").split()))
+# a=merge(x,y) 
+# print("List after Sorted:",a)   
+
+# def concat(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=concat(a,b)
+# print("List After merge:", x)
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("List after merged:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("Sorted List after:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b)
+# print("List After Sorted:",x)    
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("List After Sorted:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b) 
+# print("List After Sorted:",x)   
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b)
+# print("List after sorted:",x)    
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b)
+# print("List After sorted:",x)    
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b)
+# print("List after sorted:",x)    
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split()))
+# b=list(map(int, input("Enter list 2:").split()))
+# x=merge(a,b)
+# print("List After Sorted:",x)    
+
+# def merge(s1,s2):
+#     s=s1+s2
+#     s.sort()
+#     return s
+# a=list(map(int, input("Enter List 1:").split())) 
+# b=list(map(int, input("Enter List 2:").split()))
+# x=merge(a,b)
+# print("List after sorted:",x)   
+
 def merge(s1,s2):
     s=s1+s2
     s.sort()
     return s
 a=list(map(int, input("Enter List 1:").split()))
 b=list(map(int, input("Enter List 2:").split()))
-x=merge(a,b) 
-print("Sorted after:",x)   
+x=merge(a,b)
+print("List after sorted:",x)
