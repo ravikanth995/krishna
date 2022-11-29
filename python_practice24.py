facd538cea936c45fdbfaafb8992b36774581a17
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-29 22:57:15 +0530

    Write a python class to implement power as pow(x,n)

---
+++ python_practice24.py
@@ -0,0 +1,199 @@
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x Value:"))
+# n=int(input("Enter n Value:"))        
+# p.pow(x,n)
+
+# class power():
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter N value:"))        
+# p.pow(x,n)
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter N value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))        
+# n=int(input("Enter N value:"))
+# p.pow(x,n)
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()        
+# x=int(input("Enter X value:")) 
+# n=int(input("Enter N value:"))
+# p.pow(x,n)       
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter N value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter N value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:")) 
+# n=int(input("Enter N value:"))
+# p.pow(x,n)       
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X Value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X Value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X Value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter  x value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter N Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter X value:"))
+# n=int(input("Enter n Value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:"))
+# n=int(input("Enter n value:"))
+# p.pow(x,n)        
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:"))
+# n=int(input("Enter n value:")) 
+# p.pow(x,n)       
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:")) 
+# n=int(input("Enter n value:")) 
+# p.pow(x,n)      
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:")) 
+# n=int(input("Enter n value:"))
+# p.pow(x,n)       
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter  x value:"))
+# n=int(input("Enter n value:"))
+# p.pow(x,n)
+
+# class power:
+#     def pow(self,x,n):
+#         print("pow(",x,",",n,")=",x**n)
+# p=power()
+# x=int(input("Enter x value:"))
+# n=int(input("Enter n value:"))
+# p.pow(x,n)
+
+class power:
+    def pow(self,x,n):
+        print("pow(",x,",",n,")=",x**n)
+p=power()
+x=int(input("Enter x value:")) 
+n=int(input("Enter n value:"))
+p.pow(x,n)       
