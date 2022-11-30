c5012bc21ff6fd07ea778de21fbc096dfd90ce37
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-30 11:05:59 +0530

    Write a python class to reverse a string word by word

---
+++ python_practice25.py
@@ -0,0 +1,172 @@
+
+# sentence = "dread it run from it destiny still arrives"
+# print(" ".join(sentence.split()[::-1]))
+
+# sentence = input("Enter a sentence :")
+# print(" ".join(reversed(sentence.split())))
+
+# sentence=input("Enter the sentence:")
+# print(" ".join(reversed(sentence.split())))
+
+# sentence=input("Enter The Sentence:")
+# print(" ".join(reversed(sentence.split())))
+
+# class reverse:
+#     def rev_setence(self,sentence):
+#         words= sentence.split(" ")
+#         reverse_setn=" ".join(reversed(words))
+#         print(reverse_setn)        
+# c=reverse()
+# c.rev_setence(input("Enter the string:"))
+
+# class reverse:
+#     def rev_sentence(self, sentence):
+#         words=sentence.split(" ")
+#         reverse_sentence=" ".join(reversed(words))
+#         print(reverse_sentence)
+# c=reverse()
+# c.rev_sentence(input("Enter the Sentence:")) 
+
+# class reverse:
+#     def rev_sentence(self, sentence):
+#         words=sentence.split(" ")
+#         reverse_sentence=" ".join(reversed(words))
+#         print(reverse_sentence)
+# c=reverse()
+# c.rev_sentence(input("Enter the sentence:"))
+
+# class reverse:
+#     def rev_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         sentence_reverse=" ".join(reversed(words))
+#         print(sentence_reverse)
+# c=reverse()
+# c.rev_sentence(input("Enter The Sentence:"))
+
+# class reverse:
+#     def rev_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         sentence_reverse=" ".join(reversed(words))
+#         print(sentence_reverse)
+# c=reverse()
+# c.rev_sentence(input("Enter String:")) 
+
+# class reverse:
+#     def rev_sentence(self, sentence):
+#         words=sentence.split(" ")
+#         sentence_reverse=" ".join(reversed(words))
+#         print(sentence_reverse)
+# c=reverse()
+# c.rev_sentence(input("Enter String:"))    
+
+# class reverse:
+#     def rev_sentence(self, sentence):
+#         words=sentence.split(" ")
+#         sent_reverse=" ".join(reversed(words))
+#         print(sent_reverse) 
+# c=reverse()
+# c.rev_sentence(input("Enter String:"))           
+
+# class reverse:
+#     def rev(self,sentence):
+#         words=sentence.split(" ")
+#         sent_rev=" ".join(reversed(words))
+#         print(sent_rev)
+# c=reverse()
+# c.rev(input("Enter string:"))        
+
+# class reve:
+#     def rev(self, sentence):
+#         w=sentence.split(" ")
+#         rev_sent=" ".join(reversed(w))
+#         print(rev_sent)
+# c=reve()
+# c.rev(input("Enter Sentence:"))
+
+# class reverse:
+#     def rev_word(self, sentence):
+#         words=sentence.split(" ")
+#         rev_sent=" ".join(reversed(words))
+#         print(rev_sent)
+# c=reverse()
+# c.rev_word(input("Enter Words:"))
+
+# class reverse:
+#     def reverse_statement(self,sentence):
+#         words=sentence.split(" ")
+#         rev_state=" ".join(reversed(words))
+#         print(rev_state)
+# c=reverse()
+# c.reverse_statement(input("Enter Sentence:"))
+
+# class reverse:
+#     def reverse_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_string=" ".join(reversed(words))
+#         print(reverse_string)
+# c=reverse()
+# c.reverse_sentence(input("Enter String:"))      
+
+# class reverse:
+#     def reverse_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words)
+# c=reverse()
+# c.reverse_sentence(input("Enter the String:"))          
+
+# class reverse:
+#     def reverse_words(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_sentence=" ".join(reversed(words))
+#         print(reverse_sentence)
+# c=reverse()
+# c.reverse_words(input("Enter String:"))
+
+# class reverse:
+#     def reverse_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words)
+# c=reverse()
+# c.reverse_sentence(input("Enter String:"))        
+
+# class reverse:
+#     def reverse_sentence(self, sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words)
+# c=reverse()
+# c.reverse_sentence(input("Enter Strinng:"))
+
+# class reverse:
+#     def reverse_sentence(Self, sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words)
+# c=reverse()
+# c.reverse_sentence(input("Enter String:"))        
+
+# class reverse:
+#     def reverse_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words)
+# c=reverse() 
+# c.reverse_sentence(input("Enter String:"))      
+
+# class reverse:
+#     def reverse_sentence(self,sentence):
+#         words=sentence.split(" ")
+#         reverse_words=" ".join(reversed(words))
+#         print(reverse_words) 
+# c=reverse()
+# c.reverse_sentence(input("Enter String:"))
+
+class reverse:
+    def reverse_sentence(self,sentence):
+        words=sentence.split(" ")
+        reverse_words=" ".join(reversed(words))
+        print(reverse_words)
+c=reverse()
+c.reverse_sentence(input("Enter String:"))        
