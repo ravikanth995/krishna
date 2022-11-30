3982f453d74ef9b7d346c009dd88e166025f0b43
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-30 12:07:12 +0530

    Write a program to demonstrate Try/except and finally block

---
+++ python_practice26.py
@@ -0,0 +1,146 @@
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be possible (b=0):")
+# else:
+#     print("a//b value is:",c)   
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:")) 
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be possible (b==0):")
+# else:
+#     print("a//b value is:",c)      
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:")) 
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be possible (b==0):")
+# else:
+#     print("a//b value is :",c)    
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division is not possible as (b==0):")
+# else:
+#     print("The Value of a//b is:",c)    
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division is not possible as (b==0:")
+# else:
+#     print("The Value of the Division is:",c)
+
+# try:
+#     a=int(input(("Enter a Value:")))
+#     b=int(input(("Enter b Value:")))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division is not possible as(b==0):") 
+# else:
+#     print("Division value is:",c)      
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("division can't be done as (b==0):") 
+# else:
+#     print("The Value of a//b is:",c)    
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be done as (b==0):")
+# else:
+#     print("The Value of a//b is:",c)            
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be possible as (b==0):")
+# else:
+#     print("The Value of a//b is:",c)    
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be done as (b==0):")
+# else:
+#     print("The Value of a//b:",c)        
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("division can't be done as (b==0):")
+# else:
+#     print("The division value is:",c)        
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("Division can't be done as (b==0):")
+# else:
+#     print("The value of a//b is:",c)        
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("division error as b==0:")
+# else:
+#     print("The Value of a//b:",c)  
+
+# try:
+#     a=int(input("Enter a value:"))
+#     b=int(input("Enter b VAlue:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("division is not possible:")
+# else:
+#     print("Division value is:",c)              
+
+# try:
+#     a=int(input("Enter a Value:"))
+#     b=int(input("Enter b Value:"))
+#     c=a//b
+# except ZeroDivisionError:
+#     print("The Divsion can't be done as (b==0):") 
+# else:
+#     print("Division value is :",c)   
+
+try:
+    a=int(input("Enter a Value:"))
+    b=int(input("Enter b Value:")) 
+    c=a//b
+except ZeroDivisionError:
+    print("The Denominator must not be 0")
+else:
+    print("The value of a//b:",c)
+finally:
+    print("End of program")               
+
