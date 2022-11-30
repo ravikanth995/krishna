f559a3130d2f5ee3d38a143e7e06982c35ecd080
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-30 22:17:52 +0530

    Format and eval

---
+++ python_practice28.py
@@ -0,0 +1,37 @@
+# ver=3.11
+# txt="Python\'s latest version is {}"
+# print(txt.format(ver))
+
+# English_Marks=87
+# Hindi_Marks=49
+# Science_Marks=98
+# History_Marks=78
+# Social_Science=86
+# Mathematics_Marks=83
+# Student_Name="Ravikanth"
+# Id_Number=10823
+# Average_Marks=(English_Marks+Hindi_Marks+Science_Marks+Social_Science+History_Marks+Mathematics_Marks)/6
+# txt="My Name is {}, My Id Number is {}, My English Marks is {}, Hindi Marks is {}, Science Marks is {}, History Marks is {}, Social Science Marks is {}, Mathmatics Marks is {}"
+# print(txt.format(Student_Name,Id_Number, English_Marks, Hindi_Marks, Science_Marks,History_Marks,Social_Science,Mathematics_Marks, Average_Marks))
+
+
+# E_marks=56
+# H_marks=9
+# M_marks=76
+# avg_marks=(E_marks+H_marks+M_marks)/3
+# txt="English MArks is {}, Hindi Marks is {}, MAth marks is {}, Average marks is {}"
+# # print(txt.format(E_marks,H_marks,M_marks,avg_marks))
+
+# e_Marks=98
+# h_Marks=33
+# s_marks=88
+# sum=(e_Marks+h_Marks+s_marks)
+# avg=(e_Marks+h_Marks+s_marks)/3
+# txt="My English Marks is {}, Hindi marks is {}, Science Marks is {}, Sum of subjects is {}, and Average is {}"
+# print(txt.format(e_Marks,h_Marks,s_marks,sum,avg))
+
+
+
+#Eval() function is used to evaluate the value of string. it takes a string as an argument , evaluates string as number and returns the numeric value"
+eval('33**2')
+print(eval)
