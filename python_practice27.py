7bad4b90b5d4edb0307b0c8949c7f8b22d138e79
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-11-30 12:28:53 +0530

    Write a program that has with and as a=or try, finally

---
+++ python_practice27.py
@@ -0,0 +1,53 @@
+# file=open("file1.txt","w")
+# try:
+#     file.write("hello brother Ravikanth!")
+# finally:
+#  file.close()
+# with open("file2.txt","w") as file:
+#     file.write("hello friend ravikanth") 
+
+# file=open("ravi.txt","w")
+# try:
+#     file.write("Hello brother ! whatsapp")
+# finally:
+#  file.close()
+# with open("ravi1.txt","w") as file:
+#     file.write("hi ravikanth")
+
+# file=open("ravi2.txt","w")
+# try:
+#     file.write("Your File is been created here")
+# finally:
+#     file.close()
+# with open("ravi3.txt",'w') as file:
+#     file.write("And again your file created here.. you can append here anything that is not objectionable")      
+
+# file=open("ravi4.txt","w")
+# try:
+#     file.write("ravikanth your file is being created here!")
+# finally:
+#     file.close()
+# with open("ravi5.txt","w") as file:
+#     file.write("hi")
+
+# file=open("ravi6.txt","w")
+# try: 
+#     file.write("Sonu Nigam")
+# finally:
+#     file.close()
+# with open("ravi7.txt","w") as file:
+#     file.write("Akshay Kumar")
+
+file=open("write.txt","w")
+try:
+    file.write("Now ! i am listening to Songs")
+finally:
+    file.close()
+with open("write1.txt","w") as file:
+    file.write("It is of Sonu Nigam called Ghum Shuda")
+
+
+
+
+
+    
