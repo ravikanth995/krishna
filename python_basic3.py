43de86e0025c60b81d978816257b3452354266a2
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-01 12:30:57 +0530

    Areea of Circle and Circumference using float input method

--- python_basic3.py
+++ python_basic3.py
@@ -168,9 +168,23 @@
 # print("Area of the Circle=%.2f"%area)
 # print("Circumference of the Circle is=%.3f"%circumference)
 
+# pi=3.142
+# r=float(input("Enter Radius Value:"))
+# area=pi*r*r
+# circumference=2*pi*r
+# print("Area of the circle %.3f"%area)
+# # print("Circumference of the Circle %.3f"%circumference)
+
+# pi=3.142
+# r=float(input("Enter Radius Value:"))
+# area=pi*r*r
+# circumference=2*pi*r
+# print("Area of the Circle is %.2f"%area)
+# print("Circumference of the Circle is%.2f"%circumference)
+
 pi=3.142
 r=float(input("Enter Radius Value:"))
 area=pi*r*r
 circumference=2*pi*r
-print("Area of the circle %.3f"%area)
-print("Circumference of the Circle %.3f"%circumference)
+print("Area of the Circle =%.3f"%area)
+print("Circumference of the Circle =%.3f"%circumference)
---
+++ python_practice29.py
@@ -0,0 +1,79 @@
+# p=float(input("Enter principle Amount:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter rate percent:"))
+# si=(p*t*r)/100
+# total_amount=p+si
+# print("*"*30)
+# print("Interest on Amount=:",si)
+# print("Total Amount :=", total_amount)
+# print("*"*30)
+
+# p=float(input("Enter Principle Amount:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter Rate of Interest:"))
+# si=(p*r*t)/100
+# total_amount=p+si
+# print("*"*35)
+# print("interest on Amount=:",si)
+# print("total Amount=:",total_amount)
+# print("*"*22)
+
+# p=float(input("Enter Principle Amount:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter Rate of Interest:"))
+# si=(p*t*r)/100
+# total_amount=p+si
+# print("*"*38)
+# print("Interest on principle is:",si)
+# print("Total Amount is:",total_amount)
+# print("*"*34)
+
+# p=float(input("Enter Principle Value:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter Rate of Interest:"))
+# si=(p*r*t)/100
+# Total_amount=p+si
+# print("*"*25)
+# print("Interest on the Principal Amount is:",si)
+# print("Total Amount is :",Total_amount)
+
+# p=float(input("Enter Princple Amount:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter Rate of Interest:"))
+# si=(p*r*t)/100
+# Total=p+si
+# print("*"*40)
+# print("Interest Amount is :",si)
+# print("Total Amount is:",Total)
+
+# p=float(input("Enter Principal Amount:"))
+# t=float(input("Enter Time Period:"))
+# r=float(input("Enter Rate of Interest:"))
+# si=(p*r*t)/100
+# Total_amount=p+si
+# print("Interest on principal amount is:",si)
+# print("Total amount is:",Total_amount)
+
+# p,t,r=float(input("Enter Principal:")), float(input("Enter Time period:")), float(input("Enter Rate:"))
+# si=p*r*t/100
+# total=p+si
+# print("interest is:",si)
+# print("total is:",total)
+
+# p,t,r=float(input("Enter principal Amount:")), float(input("Enter Time:")), float(input("Enter Rate:"))
+# si=p*r*t/100
+# total=p+si
+# print("Interest on principal:",si)
+# print("total amount:",total)
+
+# p,r,t=float(input("Enter Principal value:")), float(input("Enter Time period:")), float(input("Enter Rate Percent:"))
+# si=p*r*t/100
+# total=si+p
+# print("interest on principal amount:",si)
+# print("total amount is:",total)
+
+p,r,t=float((input("Enter Principal amount:")),(input("Enter rate percent:")),(input("Enter Time period:")))
+si=p*r*t/100
+total=si+p
+print("Interest on principal amount is:",si)
+print("Total amount is:",total)
