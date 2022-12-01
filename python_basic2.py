d5839db360cc84a8d17c621dbf2bc2e1cb446797
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-01 11:12:34 +0530

    Input and Format

---
+++ python_basic2.py
@@ -0,0 +1,20 @@
+# a=int(input("Enter First Integer:"))
+# b=int(input("Enter Second Integer:"))
+# c=int(input("Enter Third Integer:"))
+# sum=a+b+c
+# avg=sum/3
+# print("Sum of all Three Integer is:",sum)
+# print("Average of All Three Ineteger is:",avg)
+
+# a=float(input("Enter FIrst Float Value:"))
+# b=float(input("Enter Second Float Value:"))
+# c=float(input("Enter Third Float Value"))
+# sum=a+b+c
+# average=sum/3
+# print("Sum of all three float is:",sum)
+# print("Average of all three Float value is:",average)
+
+Name=input("Enter Name:")
+age=int(input("Enter Age:"))
+Address=input("Enter Address here:")
+print("Name:{}, age{},, Address:{}".format(Name,age,Address))
