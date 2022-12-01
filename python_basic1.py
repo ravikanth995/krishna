9a56f080cb3391930f70854582293c2eb9ecc086
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-01 10:33:38 +0530

    String into Integer then total them together

---
+++ python_basic1.py
@@ -0,0 +1,25 @@
+# #Explicite Type Conversion
+# a=144
+# b="561"
+# #We have to convert String to Integer by Explicite Type Casting 
+# b=int(b)
+# c=a+b
+# print(c)
+
+# a=12234
+# b="12234"
+# b=int(b)
+# total=a+b
+# print(total)
+
+# a=123
+# b="143"
+# b=int(b)
+# c=a+b
+# print(c)
+
+a=124
+b="123"
+b=int(b)
+c=a+b
+print(c)
