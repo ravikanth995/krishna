ddb3c133b396a68cf63645c00f06dd3c22330c8c
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-02 23:23:48 +0530

    Swapping of Integer

--- python_basic6.py
+++ python_basic6.py
@@ -67,6 +67,23 @@
 # num_sqrt=num**0.5
 # print("Square root of %.0f is:%.2f"%(num,num_sqrt))
 
-num=float(input("Enter Number:"))
-num_sqrt=num**0.5
-print("Square root of %.0f is:%.2f"%(num,num_sqrt))
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is:%.2f"%(num,num_sqrt)
+
+
+def twoSum(self, nums, target):
+        # two point
+        nums_index = [(v, index) for index, v in enumerate(nums)]
+        nums_index.sort()
+        begin, end = 0, len(nums) - 1
+        while begin < end:
+            curr = nums_index[begin][0] + nums_index[end][0]
+            if curr == target:
+                return [nums_index[begin][1], nums_index[end][1]]
+            elif curr < target:
+                begin += 1
+            else:
+                end -= 1
+
+twoSum(1,2,3,4,5,6,7,target=15)       
---
+++ python_basic7.py
@@ -0,0 +1,63 @@
+# #Swapping of Two Variables
+# x=input("Enter Value of X:")
+# y=input("Enter Value of Y:")
+# print("Before Swapping of X :", x, "\nY value is :",y)
+# temp=x
+# x=y
+# y=temp
+# print("The value of x after swapping is:{0}, after swapping of y is:{1}".format(x,y))
+
+# x=int(input("Enter Value of X:"))
+# y=int(input("Enter Valule of Y:"))
+# print("Before Swapping of X:",x, "\nBefore Swapping of Y is:",y)
+# temp=x
+# x=y
+# y=temp
+# print("After Swapping of X is :{0}, \nAfter Swapping of Y is:{1}".format(x,y))
+
+# x=int(input("Enter Value of X:"))
+# y=int(input("Enter Value of Y:"))
+# print("Before Swapping of X is:",x, "\nBefore Swapping of Y is:",y)
+# Z=x
+# x=y
+# y=Z
+# print("After Swapping of X is:{0}, \nAfter swapping of Y is:{1}".format(x,y))
+
+# x=int(input("Enter Value of X:"))
+# y=int(input("Enter Value of Y:"))
+# print("Before swapping of X:",x, "\nBefore Swapping of Y is:",y)
+# temp=x
+# x=y
+# y=temp
+# print("After Swapping of X Value:{0}, \nAfter Swapping of Y is:{1}".format(x,y))
+
+# x=int(input("Enter The Value of X:"))
+# y=int(input("Enter The Value of Y:"))
+# print("Before Swapping of X is",x, "\nAfter Swapping of Y is:",y)
+# a=x
+# x=y
+# y=a
+# print("After Swapping of X is:{0}, \nAfter Swapping of Y is:{1}".format(x,y))
+
+# x=int(input("Enter X Value:"))
+# y=int(input("Enter Y VAlue:"))
+# print("Before Swapping of X is:",x, "\nBefore Swapping of Y is:",y)
+# temp=x
+# x=y
+# y=temp
+# print("After Swapping of X is:{0}, \nAfter SWappinng of Y is:{1}".format(x,y))
+
+# x=input("Enter X value:")
+# y=input("Enter Y value:")
+# str1=x+y
+# print(str1)
+
+x=int(input("Enter X Value:"))
+y=int(input("Enter Y Value:"))
+print("Before Swapping of X is:",x, "\nbefore Swapping of Y is:",y)
+temp=x
+x=y
+y=temp
+print("After Swapping of X is:{0}, \nAfter Swapping of Y is:{1}".format(x,y))
+
+# x=int(input("Enter X value:"))
