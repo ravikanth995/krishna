8c1f93aaa1ad8b78027c2bfb90c00d41c10fff6b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-02 10:43:50 +0530

    Enter Number and Display its Cube and Square Number

---
+++ python_basic4.py
@@ -0,0 +1,31 @@
+# with open("test.txt","w") as file:
+#     file.write("It is a keyword")
+
+# ver=3.11
+# txt="Python Version is {}"
+# print(txt.format(ver))
+
+# num=int(input("Enter Number:"))
+# cube=num*num*num
+# square=num*num
+# print("The Cube of the given number is :{1} \nSquare of the number is:{2}".format(num, cube, square))
+
+# num=int(input("Enter Any Number:"))
+# Cube= num*num*num
+# Square=num*num
+# print("Cube of the Number is :{1} \nSqaure of the Number is :{2}".format(num, Cube, Square))
+
+# num=int(input("Enter Any Number:"))
+# cube=num*num*num
+# square=num*num
+# print("Cube of the Given Number is:{1} \nSquare of the Given Number is :{2}".format(num,cube,square))
+
+# num=int(input("Enter Any Number:"))
+# Cube=num*num*num
+# Square=num*num
+# print("Cube of the Given Number is :{1} \nSqaure of the Given Number is:{2}".format(num,Cube,Square))
+
+num=int(input("Enter Any Number:"))
+Cube=num*num*num
+Square=num*num
+print("The Cube of the Given NUmber is:{1} \nSquare of the Given Number is:{2}".format(num, Cube,Square))
