6e115131726ddcabfa7dd20114c1b2f5f6ba61ab
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-02 12:25:55 +0530

    Find Square root of any given number y by using input method

---
+++ python_basic6.py
@@ -0,0 +1,72 @@
+# import math
+# print(math.sqrt(225))
+
+# import math
+# print(math.sqrt(16))
+
+# import math
+# print(math.sqrt(289))
+
+# import math
+# print(math.sqrt(361))
+
+# import math
+# print(math.sqrt(900))
+
+# import math
+# print(math.sqrt(525))
+
+# import math
+# print(math.sqrt(625))
+
+# num=float(input("Enter a number:"))
+# num_sqrt=num**0.5
+# print("The Square root of %.0f is: %.0f"%(num, num_sqrt))
+
+# num=float(input("Enter a Number:"))
+# num_sqrt=num**0.5
+# print("The Square root of %.1f number is %.2f"%(num,num_sqrt))
+
+# num=float(input("Enter Number of Square root you want:"))
+# num_sqrt=num**0.5
+# print("Square root of %.3f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square of the %.3f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter the Number:"))
+# num_sqrt=num**0.5
+# print("Square of %.2f is %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square of %.2f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square of %.2f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter  Number:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is: %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is : %.3f"%(num,num_sqrt))
+
+# num=float(input("Enter Number:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is: %.2f"%(num,num_sqrt))
+
+# num=float(input("Enter Number of Days:"))
+# num_sqrt=num**0.5
+# print("Square root of %.0f is:%.2f"%(num,num_sqrt))
+
+num=float(input("Enter Number:"))
+num_sqrt=num**0.5
+print("Square root of %.0f is:%.2f"%(num,num_sqrt))
