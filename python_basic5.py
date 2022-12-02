f48c9a54dd4d1719843c358ea1547e9c0a854dcc
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-02 11:55:29 +0530

    To display Months and Days for Entered number of days

---
+++ python_basic5.py
@@ -0,0 +1,104 @@
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("Months=:", int(months),"days=:",days)
+ 
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("Months=:", int(months),"days=:",days) 
+
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("months=", int(months), "days=",days)
+
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("Month is=", int(months), "days=",days)
+
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("month is:=", int(months), "days=",days)
+
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("Month is:", int(months), "days",days)
+
+# days=int(input("Enter Number of Days:"))
+# months=days/30
+# days=days%30
+# print("Months is :", int(months), "Days :", days)
+
+# days=int(input("Enter Number of Days:"))
+# Months=days/30
+# days=days%30
+# print("Month is:", int(Months), " \nDay is:",days)
+
+# days=int(input("Enter Number of Days:"))
+# Months=days/30
+# days=days%30
+# print("Month is=", int(Months), "\nDay are/is is=", days)
+
+# Days=int(input("Enter Number Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Month is=", int(Months), "\nDays=",days)
+
+# days=int(input("Enter Number of Days:"))
+# Months=days/30
+# Days=days%30
+# print("Months are/is=", int(Months), "\nDays are\is=",Days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Month is/are=", int(Months), "\nDays are/is=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Months are/is=", int(Months), "\nDays are/is=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Months are/is=", int(Months), "\nDays is=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Months are/is=", int(Months), "\nDay is=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Months=",int(Months), "\nDays=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# Months=Days/30
+# days=Days%30
+# print("Month is=",int(Months), "\nDays=",days)
+
+# Days=int(input("Enter Number of days:"))
+# Months=Days/30
+# days=Days%30
+# print("Month is=",int(Months),"\nDays=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# months=Days/30
+# days=Days%30
+# print("Month=",int(months),"\nDays=",days)
+
+# Days=int(input("Enter Number of Days:"))
+# months=Days/30
+# days=Days%30
+# print("Months=",int(months),"\nDays=",days)
+
+Days=int(input("Enter Number of Days:"))
+month=Days/30
+days=Days%30
+print("Month=",int(month),"\nDays=",days)
