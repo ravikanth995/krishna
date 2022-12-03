3b48b8588d00800cdefc836162e90d8e2410f4ac
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-03 12:22:54 +0530

    Find your Grade

---
+++ python_basic_Looping.py
@@ -0,0 +1,78 @@
+# #Conditional Statement
+# val=int(input("Enter number list:"))
+# if val>0:
+#     print("positive Number:")
+# else:
+#     print("Negative Number:")
+
+# num=int(input("Enter Number:"))
+# if num%2==0:
+#     print(":Is a Even NUmber")
+# else:
+#     print("odd Number")    
+
+# a=int(input("Enter a value:"))
+# b=int(input("Enter b Value:"))
+# c=int(input("Enter c value:"))
+# if a>b and a>c:
+#     print("A is greater than B & C")
+# elif b>c:
+#     print("B is greater than C")     
+# else:
+#     print("C is greater")    
+
+# a=int(input("Enter a Value:"))
+# b=int(input("Enter b Value:"))
+# c=int(input("Enter c Value:"))
+# if a>b and a>c:
+#     print("A is greater than B and C")
+# elif b>c:
+#     print("B is greater than C")
+# else:
+#     print("C is Greater")        
+
+
+# a=int(input("Enter a value:"))
+# # b=int(input("Enter b Value:"))
+# if a>0:
+#     print("A is Positive Integer")
+# elif a<0:
+#     print("A is Negative Integer") 
+# else:
+#     print("A is Neutral Integer")       
+
+# Grade=int(input("Enter Your Marks:"))
+# if Grade>=90:
+#     print("A+")
+# elif Grade>=80:
+#     print("A Grade")
+# elif Grade>=70:
+#     print("B Grade")
+# elif Grade>=60:
+#     print("C Grade") 
+# else:
+#     print("Better Luck ! Next time")               
+
+# Grade=int(input("Enter Grade:"))
+# if Grade>=90:
+#     print(" Grade is A+")
+# elif Grade>=80:
+#     print("Grade is A")
+# elif Grade>=70:
+#     print("Grade is B") 
+# else:
+#     print("Grade is Lol")           
+
+Grade=int(input("Enter Grade :"))
+if Grade>=90:
+    print("Grade is A+")
+elif Grade>=80:
+    print("Grade is A")
+elif Grade>=70:
+    print("Grade is B")
+elif Grade>=60:
+    print("Grade is C")
+elif Grade>=59:
+    print("C grade")   
+else:
+    print("Failed !")                 
