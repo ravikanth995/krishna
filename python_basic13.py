07aa5e5dbeb9ddd59ad27ec1244d627f9d2c515b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-03 22:01:24 +0530

    nested if else

---
+++ python_basic13.py
@@ -0,0 +1,52 @@
+# grade=int(input("Enter Grade Marks:"))
+# if grade>=90:
+#     print("Grade is Rank")
+# else:
+#     if grade>=80:
+#         print("Grade is A")
+#     else:
+#         if grade>=70:
+#             print("Grade is B")
+#         else:
+#             if grade>=60:
+#                 print("Grade is C")
+#             else:
+#                 print("Failed bro!")                
+         
+# Grade=int(input("Enter Grade Marks:"))
+# if Grade>=90:
+#     print("A+")
+# else:
+#     if Grade>=80:
+#         print("Grade is A")
+#     else:
+#         if Grade>=70:
+#             print("Grade is B+")
+#         else:
+#             if Grade>=60:
+#                 print("Grade is B")
+#             else:
+#                 if Grade>=50:
+#                     print("Grade is C")
+#                 else:
+#                     print("Failed")                
+
+
+Grade=int(input("Enter Grade Marks:"))
+if Grade>=90:
+    print("Grade is A+")
+else:
+    if Grade>=80:
+        print("Grade is A")
+    else:
+        if Grade>=70:
+            print("Grade is B+")
+        else:
+            if Grade>=60:
+                print("Grade is C+")
+            else:
+                if Grade<=50:
+                    print("Grade is C")
+                else:
+                    if Grade<=35:
+                        print("Failed")                    
