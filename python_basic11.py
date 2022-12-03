521b0774a880c66c9e5715a50a71ec7899141075
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-03 11:39:20 +0530

    Convert Celsius into Farhenheit

---
+++ python_basic11.py
@@ -0,0 +1,23 @@
+# Celsius=float(input("Enter Celsius :"))
+# Fahrenheit=(Celsius*1.8)+32
+# print("%.0f Celsius is Equal to Farhenheit of:%.4f"%(Celsius,Fahrenheit))
+
+# Celsius=float(input("Enter Celsius Value:"))
+# far=(Celsius*1.8)+32
+# print("%.0f Degree of Celsius is equal to :%.2f  of Farhenheit"%(Celsius,far))
+
+# C=float(input("Enter Celcius:"))
+# F=(C*1.8)+32
+# print("%.2f Degree Celsius is Equal to :%.2f Farhenheit"%(C,F))
+
+# C=float(input("Enter Celsius:"))
+# Far=(C*1.8)+32
+# print("%.2f Degree of Celsius Is Equal To %.2f of Farhenheit:"%(C,Far))
+
+# c=float(input("Enter Celsius Value:"))
+# Far=(c*1.8)+32
+# print("%.2f of Degree celsius is Equal to :%.2f of Farhenheit"%(c,Far))
+
+c=float(input("Enter celsius Value:"))
+far=(c*1.8)+32
+print("%.0f Degree of celsius is Equal to %.3f"%(c,far))
