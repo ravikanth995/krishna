2dda04c76842882124a2fb572a389ba62ecfdd08
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-04 12:21:37 +0530

    Write a program to print sum of n natural number(sum=1+2+3+4+5..+n) using Input function

---
+++ python_basic15.py
@@ -0,0 +1,203 @@
+# val=int(input("Enter a Number:"))
+# #Initialize Sum and Counter
+# sum=0
+# i=1
+# while i<=val:
+#     sum=sum+i
+#     i=i+1
+# print("The Sum is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=1
+# print(0)
+# while i<=val:
+#      sum=sum+i
+#      print(i)
+#      i=i+1
+# print("Sum of n Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=0
+# # print(0)
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of n Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# print("*"*27)
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+1
+#     print(i)
+#     i=i+1
+# # print("Sum of Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter a Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number:",sum)
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# print("*"*30)
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print('Sum of Natural Number:',sum)
+# print("*"*20)
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("The Sum of Number is:",sum)
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# # print("Sum of Number:",sum)    
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum of Number is:",sum)    
+
+# val=int(input("Enter Any Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("The Sum of Number is:",sum)
+
+# val=int(input("Enter Any Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum is:",sum)  
+
+# val=int(input("Enter Number:")) 
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("The Sum is:",sum)     
+
+# x=int(input("Enter Any Number:"))
+# sum=0
+# i=0
+# while i<=x:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Enter Number is:",sum)    
+
+# x=int(input("Enter any Number:"))
+# sum=0
+# i=0
+# while i<=x:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("Sum is:",sum)    
+
+# val=int(input("Enter Number:"))
+# sum=0
+# i=0
+# while i<=val:
+#     sum=sum+i
+#     print(i)
+#     i=i+1
+# print("The Sum is:",sum)    
+
+val=int(input("Enter Number:"))
+sum=0
+i=0
+while i<=val:
+    sum=sum+i
+    print(i)
+    i=i+1
+print("Sum is:",sum)    
+
