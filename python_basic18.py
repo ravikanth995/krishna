12db1bb08b23a71769a7ecac800438c35120f1ee
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-04 19:13:05 +0530

    Write a program to find Factorial of given number using Input function method

---
+++ python_basic18.py
@@ -0,0 +1,153 @@
+# # fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)   
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:")) 
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Given Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The factoral is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factoiral is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     print(i)
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     print(i)
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Numbere:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Factorial is:",fact)    
+
+# fact=1
+# i=1
+# num=int(input("Enter Number:"))
+# while i<=num:
+#     fact=fact*i
+#     i=i+1
+# print("The Facto is:",fact)    
+
+fact=1
+i=1
+num=int(input("Enter Number:"))
+while i<=num:
+    fact=fact*i
+    i=i+1
+print("The Factorial is:",fact)    
