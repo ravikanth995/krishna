d7be39212724d2c290651d0327b0df3423c76203
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-04 17:03:52 +0530

    Write a program to find Sum of all Even and Odd number

---
+++ python_basic16.py
@@ -0,0 +1,260 @@
+# num=int(input("Enter any number:"))
+# count=1
+# s_even=s_odd=0
+# while (count<=num):
+#     if (count%2)==0:
+#         s_even+=count
+#     else:
+#         s_odd+=count
+#     count+=1
+
+# print("Sum of even number:",num, "is",s_even)
+# print("Sum of odd Number:",num, "is",s_odd)        
+
+# num=int(input("Enter a Number:"))
+# count=0
+# s_even=s_odd=0
+# while (count<=num):
+#     if(count%2)==0:
+#         print(count)
+#         s_even+=count
+#     else:
+#         print(count)
+#         s_odd+=count
+#     count+=1
+# print("The Sum of even number:",s_even) 
+# print("The Sum of odd number:",s_odd)           
+
+# num=int(input("Enter number:"))
+# count=1
+# s_even=s_odd=0
+# while count<=num:
+#     if(count%2)==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum of odd Number:",s_odd)        
+
+# num=int(input("Enter Number:"))
+# count=1
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of the Even Number:",s_even)
+# print("The Sum of the Odd Number:",s_odd)        
+
+# num=int(input("Enter Number:")) 
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Natural Number is:",s_even)
+# print("The Sum of Odd Natural Number is:",s_odd)               
+
+# num=int(input("Enter Number:"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum of Odd Number:",s_odd)       
+
+# num=int(input("Enter Number:"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum of Odd Number is:",s_odd)            
+
+# num=int(input("Enter Number:"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The sum of even number:",s_even)
+# print("The sum of odd number:",s_odd)      
+
+# num=int(input("Enter Number:"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum of Odd Number:",s_odd)                            
+
+# num=int(input("Enter Number:"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The sum of Odd Number:",s_odd)            
+
+# num=int(input("Enter Number:"))
+# ravi=0
+# s_even=s_odd=0
+# while ravi<=num:
+#     if ravi%2==0:
+#         s_even+=ravi
+#         print(ravi)
+#     else:
+#         s_odd+=ravi
+#         print(ravi)
+#     ravi+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum of Odd Number:",s_odd)            
+
+# num=int(input("Enter Number"))
+# count=0
+# s_even=s_odd=0
+# while count<=num:
+#     if count%2==0:
+#         s_even+=count
+#         print(count)
+#     else:
+#         s_odd+=count 
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",s_even)
+# print("The Sum od odd Number:",s_odd)        
+
+# num=int(input("Enter Number:"))
+# count=0
+# even_sum=odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         even_sum+=count
+#         print(count)
+#     else:
+#         odd_sum+=count 
+#         print(count)
+#     count+=1
+# print("The Sum of Even NUmber:",even_sum) 
+# print("The Sum of Odd Number is:",odd_sum)          
+
+# num=int(input("Enter Number:"))
+# count=0
+# even_sum=odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         even_sum+=count
+#         print(count)
+#     else:
+#         odd_sum+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",even_sum)
+# print("The Sum of odd Number:",odd_sum)
+
+# num=int(input("Enter Number:"))
+# count=0
+# Even_sum=Odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         Even_sum+=count
+#         print(count)
+#     else:
+#         Odd_sum+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",Even_sum)
+# print("The Sum of Odd Number:",Odd_sum)            
+
+# num=int(input("Enter Number:"))
+# count=0
+# Even_sum=Odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         Even_sum+=count
+#         print(count)
+#     else:
+#         Odd_sum+=count
+#         print(count)
+#     count+=1
+# print("The Sum of Even Number:",Even_sum)
+# print("The Sum of Odd Number:",Odd_sum)        
+
+# num=int(input("Enter Number:"))
+# count=0
+# Even_sum=Odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         Even_sum+=count
+#         print(count)
+#     else:
+#         Odd_sum+=count
+#         print(count)
+#     count+=1
+# print("Even Sum is:",Even_sum)
+# print("Odd sum is:",Odd_sum)                
+
+# num=int(input("Enter Number:"))
+# count=0
+# Even_sum=Odd_sum=0
+# while count<=num:
+#     if count%2==0:
+#         Even_sum+=count
+#     else:
+#         Odd_sum+=count
+#     count+=1
+# print("Even Sum is:",Even_sum)
+# print("Odd Sum is:",Odd_sum)   
+
+num=int(input("Enter Number:")) 
+count=0
+Even_sum=Odd_sum=0
+while count<=num:
+    if count%2==0:
+        Even_sum+=count
+    else:
+        Odd_sum+=count
+    count+=1
+print("The Sum of Even Number:",Even_sum)
+print("The Sum of Odd NUmber:",Odd_sum)                    
---
+++ python_basic17.py
@@ -0,0 +1,41 @@
+# rev=0
+# num=int(input("Enter number:"))
+# n=num
+# while (num):
+#     rem=num%10
+#     rev=rev*10+rem
+#     num//=10
+# print("reverse of number:",rev)
+# if n==rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")     
+
+# rev=0
+# num=int(input("Enter Number:"))
+# n=num
+# while (num):
+#     rem=num%10
+#     rev=rev*10+rem
+#     num//=10
+# print("Reverse Number is:",rev)
+# if n==rev:
+#     print("The Number is Palindrome")
+# else:
+#     print("The Number is not Palindrome")    
+
+rev=0
+num=int(input("Enter Number:"))
+n=num
+while (num):
+    rem=num%10
+    rev=rev*10+rem
+    num//=10
+print("The Reversed Number is:",rev)
+if n==rev:
+    print("Palindrome")
+else:
+    print("The Number is not Palindrome")        
+
+
+
