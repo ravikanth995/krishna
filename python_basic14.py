2b65f343406c9be73ee3b416273dc7ffb1d5d076
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-04 11:13:39 +0530

    While Loop

---
+++ python_basic14.py
@@ -0,0 +1,61 @@
+# count=0
+# print("Enter While Loop")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("Exit While Loop")      
+
+# count=0
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("Exit While loop")    
+
+# count=0
+# print("Enter While Loop")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("program Ends")    
+
+# count=0
+# print("Enter While Loop")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("Programs Ends")  
+
+# count=0
+# print("While Loop")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("While Loop ends")      
+
+# count=0
+# print("While Loop Begins")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("While Loop Exit")    
+
+# count=0
+# print("While Loop Starts")
+# while count<=5:
+#     print(count)
+#     count=count+1
+# print("While Loop Exit")    
+
+# count=0
+# print("While Loop Starts")
+# while count<=6:
+#     print(count)
+#     count=count+1
+# print("While Loop Exit")    
+
+count=0
+print("While Loop Starts")
+while count<=5:
+    print(count)
+    count=count+1
+print("Exit While Loops")    
