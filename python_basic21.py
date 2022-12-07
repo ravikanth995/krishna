c62e608c7a3fcbe13b50cf65d68b35cff67650f3
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-07 22:27:05 +0530

    Number by using while with else clause

---
+++ python_basic21.py
@@ -0,0 +1,100 @@
+# x=1
+# while x<=5:
+#     print(x)
+#     x=x+1
+# else: 
+#     print("Loop finished")    
+
+# x=1
+# while x<=19:
+#     print(x)
+#     x=x+1
+# else:
+#     print("finished")    
+
+# x=1
+# while x<=5:
+#     print(x)
+#     x=x+1
+
+# x=1
+# while x<=8:
+#     print(x)
+#     x=x+1
+# else:
+#     print("Finished")    
+
+# x=1
+# while x<=6:
+#     print(x)
+#     x=x+1
+# else:
+#     print("Finished")    
+
+# x=0
+# while x<=6:
+#     print(x)
+#     x=x+1
+# else:
+#     print("Finished")    
+
+# i=0
+# while i<=7:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Finished Loop")    
+
+# i=0
+# while i<=6:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Loop Finished")    
+
+# i=0
+# while i<=6:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Loop Finished")    
+
+# i=0
+# while i<=7:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Finished")    
+
+# i=0
+# while i<=8:
+#     print(i)
+#     i=i+1
+# else:
+#     print("Finished")    
+
+# i=0
+# while i<=7: 
+#     print(i)
+#     i=i+1
+# print("finished")    
+
+# i=0
+# while i<=6:
+#     print(i)
+#     i=i+1
+
+# i=0
+# while i<=7:
+#     print(i)
+#     i=i+1
+
+# i=0
+# while i<=8:
+#     print(i)
+#     i=i+1
+
+i=0
+while i<=7:
+    print(i)
+    i=i+1
