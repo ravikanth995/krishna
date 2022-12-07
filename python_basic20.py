d9bebaf617595673da88a1bf5a521cb184adf1bf
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-07 22:09:40 +0530

    While loop in  one line

---
+++ python_basic20.py
@@ -0,0 +1,101 @@
+# n=int(input("Enter Number:"))
+# while n>0:
+#     n-=1
+#     print(n)
+
+# n=8
+# while n>0: n-=1; print(n)
+
+# n=7
+# while n>0:
+#     n-=1
+#     print(n) 
+
+# n=int(input("Enter Number:"))
+# while n>0: n-=1; print(n)
+
+# n=int(input("Enter Number:"))
+# while n>0: n-=1; print(n)
+
+# n=int(input("Enter Number:"))
+# while n>0:
+#     n-=1
+#     print(n)
+
+# n=int(input("Enter Number:"))
+# while n>0:
+#     n-=1
+#     print(n)
+
+# n=8
+# while n>0:
+#     n-=1
+#     print(n)
+
+# n=7
+# while n>0: n-=1; print(n)
+
+# n=6 
+# while n>0: n-=1; print(n)
+
+# n=4
+# while n>0: n-=1; print(n)
+
+# n=6
+# while n>0: n-=1; print(n)
+
+# n=5
+# while n>0: n-=1; print(n)
+
+# n=12
+# while n>0: n-=1; print(n)
+
+# n=10
+# while n>0: n-=1; print(n)
+
+# n=10
+# while n>0: n-=1; print(n)
+
+# n=8
+# while n>0: n-=1; print(n)
+
+# n=int(input("Enter Number:"))
+# while n>0: n-=1; print(n)
+
+# n=7
+# while n>0: n-=1; print(n)
+
+# n=5
+# while n>0: n-=1; print(n)
+
+# n=10
+# while n>0: n-=1; print(n)
+
+# n=8
+# while n>0: n-=1; print(n)
+
+# n=6
+# while n>0: n-=1; print(n)
+
+# n=7
+# while n>0: n-=1; print(n)
+# n=20
+# while n>0: n-=1; print(n)
+
+# num=8
+# while num>0: num-=1; print(num)
+
+# num=19
+# while num: num-=1; print(num)
+
+# num=17
+# while num>0: num-=1; print(num)
+
+# num=16
+# while num>0: num-=1; print(num)
+
+# num=14
+# while num>0: num-=1; print(num)
+
+num=4
+while num>0: num-=1; print(num)
