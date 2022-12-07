c81aec1ee89f1d7e5c00ede44732fd284816ae6b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-07 22:48:53 +0530

    Print 10 to 6 numbers by using while with else clause

---
+++ python_basic22.py
@@ -0,0 +1,111 @@
+# x=10
+# while x>5:
+#     print("Value of x is:",x)
+#     x=x-1
+# else:
+#     print("Finished")    
+
+# x=18
+# while x>0:
+#     print("The Value of X is:",x)
+#     x=x-1
+
+# x=19
+# while x>0:
+#     print("The Value of X is:",x)
+#     x=x-1
+
+# x=7
+# while x>0:
+#     print("Value of X is:",x)
+#     x=x-1
+
+# x=5
+# while x>0:
+#     print(x)
+#     x=x-1
+
+# x=6
+# while x:
+#     print(x)
+#     x=x-1
+
+# x=12
+# while x>0:
+#     print("Value of X is:",x)
+#     x=x-1
+
+# x=10
+# while x>3:
+#     print("Value of X is:",x)
+#     x=x-1
+
+# x=10
+# while x>0:
+#     print("The Value of X is:",x)
+#     x=x-1
+
+# x=11
+# while x>0:
+#     print(x)
+#     x=x-1
+
+# x=12
+# while x>0:
+#     print("The VAlue of X is:",x)
+#     x=x-1
+
+# x=9
+# while x>0:
+#     print("The VAlue of X is:",x)
+#     x=x-1
+
+# x=12
+# while x>0:
+#     print("The Value of X is:",x)
+#     x=x-1
+
+# x=11
+# while x>0:
+#     print("The Value of X :",x)
+#     x=x-1
+
+# x=13
+# while x>0:
+#     print(x)
+#     x=x-1
+
+# x=12
+# while x>0:
+#     print("The Value of X is",x)
+#     x=x-1
+
+# x=9
+# while x>0:
+#     print("The Value of X:",x)
+#     x=x-1
+
+# x=11
+# while x>0:
+#     print("The Value of X :",x)
+#     x=x-1
+
+# x=11
+# while x>0:
+#     print("The Value of X is:",x)
+#     x-=1
+
+# x=12
+# while x>0:
+#     print("The Value of X:",x)
+#     x-=1
+
+# x=11
+# while x>0:
+#     print("The Value of X is:",x)
+#     x-=1
+
+x=10
+while x>0:
+    print("The Value of X is :",x)
+    x-=1
