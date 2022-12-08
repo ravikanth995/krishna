cfce48a4117883c1494fbc75ba3b73fa9f44f82e
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-08 23:06:30 +0530

    Write a program to display * in N style

---
+++ python_basic28.py
@@ -0,0 +1,23 @@
+# for row in range(6):
+#     for col in range(6):
+#         if col==0 or col==5 or (row==col and col>0 and col<5):
+#             print("*",end=" ")
+#         else:
+#             print(end=" ")
+#     print()          
+
+# for row in range(6):
+#     for col in range(6):
+#         if col==0 or col==5 or (row==col and col>0 and col<5):
+#             print("*",end=" ")
+#         else:
+#             print(end=" ")    
+#     print()        
+
+for row in range(6):
+    for col in range(9):
+        if col==0 or col==5 or (row==col and col>0 and col<5):
+            print("*",end=" ")
+        else:
+            print(end=" ")
+    print()            
