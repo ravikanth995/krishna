5a44a734477ef3501da9f64c7c528b50b18fc515
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-08 22:04:45 +0530

    Program to print sum of even number series upto 10

---
+++ python_basic25.py
@@ -0,0 +1,80 @@
+# x=int(input("Enter Number:"))
+# sum=0
+# for i in range(0,8,2):
+#         sum+=1
+#         print("Sum of Even Numbers are:",i,"is",sum)
+        
+# sum=0
+# for i in range(0,9,2):
+#          sum+=i
+#          print("The Sum of:",i,"is",sum)
+
+# sum=0
+# for i in range(0,12,2):
+#     sum+=i
+#     print("The Sum of:",i,"is",sum)
+
+# sum=0
+# for i in range(0,12,2):
+#     sum+=i
+#     print("The Sum of",i,"Is",sum)
+
+# sum=0
+# for i in range(0,12,2):
+#     sum+=i
+#     print("The Sum is:",sum)
+
+# sum=0
+# for i in range(0,8):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,9):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,8):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,12):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,7):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,6):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,5):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,8):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,6):
+#     sum+=i
+#     print(sum)
+
+# sum=0
+# for i in range(0,7):
+#     sum+=i
+#     print(sum)
+
+sum=0
+for i in range(8):
+    sum+=i
+    print(sum)
