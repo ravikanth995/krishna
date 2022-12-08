56b623c56461595265900cf13501d7b460b062f1
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-08 22:28:15 +0530

    Write a program to display the following pattern

---
+++ python_basic26.py
@@ -0,0 +1,45 @@
+# for i in range(3):
+#     for j in range(2):
+#         print(i,j)
+
+# n=int(input("Enter No. of Row:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+# n=int(input("Enter No. of Rows:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()    
+
+# n=int(input("Enter No. of Rows:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("+",end=" ")
+#     print()    
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()    
+
+# n=4
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()    
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end=' ')
+#     print()    
+
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()    
