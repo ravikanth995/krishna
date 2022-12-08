d0be143278cc9e941a5fa401c4e001ee2e0cfb99
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-08 22:55:51 +0530

    Write a program to display *s in pyramid style

---
+++ python_basic27.py
@@ -0,0 +1,98 @@
+# n=6
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=7
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=8
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=3
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print(("* ")*i)
+# n=6
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=7
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=4
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=6
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=4
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=7
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=6
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=9
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+# n=6
+# for i in range(1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("* "*i)
+
+n=7
+for i in range(1,n+1):
+    print(" "*(n-i),end=" ")
+    print("* "*i)
+
+n=2
+for i in range(1,n+1):
+    print(" "*(n-i),end=" ")
+    print("* "*i)
