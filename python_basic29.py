9b801c0a5d4d8b61e2767a468b9b9c673d702494
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-09 21:12:38 +0530

    While else clause

--- python_basic29.py
+++ python_basic29.py
@@ -86,9 +86,96 @@
 #     i+=1
 # print("Loop ended")    
 
-for i in range(18):
-    print(i)
-    if i==12:
+# for i in range(18):
+#     print(i)
+#     if i==12:
+#         break
+#     i+=1
+# print("For Loop Ended")    
+
+# for i in "Python":
+#     if i=="n":
+#         break
+#     print(i)
+# print("After for Loop")    
+
+# for i in "Ravikanth":
+#     if i=="t":
+#         break
+#     print(i)
+# print("For Loop ended")    
+
+# for i in "Budha":
+#     if i=="a":
+#         break
+#     print(i)
+# print("For Loop ended")    
+
+# for i in "Fannah":
+#     if i=="h":
+#         break
+#     print(i)
+# print("For Loop Ended")    
+
+# for i in "Upanishads":
+#     if i=="d":
+#         break
+#     print(i)
+# print("For loop ended")    
+
+# for i in "PyarKePal":
+#     if i=="l":
+#         break
+#     print(i)
+# print("Chal Nikal")    
+
+# for i in "Ramani":
+#     if i=="i":
+#         break
+#     print(i)
+# print("For Loop Ended")   
+
+# i=0
+# while i<=6:
+#     if i==4:
+#         break
+#     i+=1
+#     print(i)
+# else:
+#     print("Success")    
+
+# i=0
+# while i<=7:
+#     if i==6:
+#         break
+#     print(i)
+#     i+=1
+# else:
+#     print("Loop Ended")    
+
+# i=0
+# while i<=6:
+#     if i==5:
+#         break
+#     print(i)
+#     i+=1
+# else:
+#     print("Ended")    
+
+# i=0
+# while i<=7:
+#     if i==4:
+#         break
+#     print(i)
+#     i+=1
+# else:
+#     print("Loop ended")    
+
+i=0
+while i<=5:
+    if i==5:
         break
+    print(i)
     i+=1
-print("For Loop Ended")    
+else:
+    print("Ended")    
