9683f39e8139c2742d64914d5960c258311f51e5
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-09 19:56:19 +0530

    For Loop break

--- python_basic29.py
+++ python_basic29.py
@@ -1,16 +1,94 @@
-# #For Loop With else clause
+# i=0
+# while i<=10:
+#     print(i)
+#     if i==4:
+#         break
+#     i+=1
+# print("After While Loop")    
+
+# i=0
+# while i<=10:
+#     print(i)
+#     if i==4:
+#         break
+#     i+=1
+# print("While  Loop Over")        
+
+# i=0
+# while i<=7:
+#     print(i)
+#     if i==5:
+#      break
+#     i+=1
+# print("While loop over")    
+
+# i=0
+# while i<=9:
+#     print(i)
+#     if i==6:
+#         break
+#     i+=1
+# print("While Loop Over")    
+
+# for i in range(10):
+#     print(i)
+#     if i==7:
+#         break
+#     i+=1
+# print("For Loop Over")    
+
+# for i in range(10):
+#     print(i)
+#     if i==8:
+#         break
+#     i+=1
+# print("For Loop Over")
+
+# for i in range(10):
+#     print(i)
+#     if i==8:
+#         break
+#     i+=1
+# print("Loop Over")           
+
+# for i in range(10):
+#     print(i)
+#     if i==8:
+#         break
+#     i+=1
+# print("For Loop Over")    
+
+# for i in range(19):
+#     print(i)
+#     if i==5:
+#         break
+#     i+=1
+# print("For Loop Over")    
+
+# for i in range(10000):
+#     print(i)
+#     if i==8:
+#         break
+#     i+=1
+# print("For loop ended")    
+
+# for i in range(20000000):
+#     print(i)
+#     if i==2:
+#         break
+#     i+=1
+# print("For Loop ended")    
+
 # for i in range(5):
-#     print(i, sep=" ,")
-# else:
-#     print("For loop over")    
+#     print(i)
+#     if i==4:
+#         break
+#     i+=1
+# print("Loop ended")    
 
-# for i in range(6):
-#     print(i, sep=",")
-# else:
-#     print("For loop over")    
-
-for i in range(6):
-    print(i, sep=",")
-else:
-    print("Loop Over")    
-
+for i in range(18):
+    print(i)
+    if i==12:
+        break
+    i+=1
+print("For Loop Ended")    
