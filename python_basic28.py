38e22dd0e1ccf9bdb07bb154ccd68c083b958bb8
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-09 19:12:48 +0530

    N shaped Star*

--- python_basic28.py
+++ python_basic28.py
@@ -14,10 +14,26 @@
 #             print(end=" ")    
 #     print()        
 
+# for row in range(6):
+#     for col in range(9):
+#         if col==0 or col==5 or (row==col and col>0 and col<5):
+#             print("*",end=" ")
+#         else:
+#             print(end=" ")
+#     print()            
+
+# for row in range(6):
+#     for col in range(7):
+#         if col==0 or col==5 or (row==col and col>0 and col<5):
+#             print("*",end=" ")
+#         else:    
+#             print(end=" ")
+#     print()        
+# This is The actual Star * shaped N
 for row in range(6):
-    for col in range(9):
-        if col==0 or col==5 or (row==col and col>0 and col<5):
-            print("*",end=" ")
+    for col in range(7):
+        if col==0 or col==5 or (col==row and col>0 and col<5):
+            print(end="*")
         else:
             print(end=" ")
     print()            
