32245cac93d6d3306a3a1e2a0929178839f4559d
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-09 19:38:41 +0530

    For loop with else clause

---
+++ python_basic29.py
@@ -0,0 +1,16 @@
+# #For Loop With else clause
+# for i in range(5):
+#     print(i, sep=" ,")
+# else:
+#     print("For loop over")    
+
+# for i in range(6):
+#     print(i, sep=",")
+# else:
+#     print("For loop over")    
+
+for i in range(6):
+    print(i, sep=",")
+else:
+    print("Loop Over")    
+
