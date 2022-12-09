3a94580a88b285ae4db958dce7bc92ff051f1401
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-09 22:52:40 +0530

    Write a program to count the number of digits in the entered number

---
+++ python_basic31.py
@@ -0,0 +1,104 @@
+# n=int(input("Enter Number:"))
+# count=0
+# while n>0:
+#     count+=1
+#     n=n//10
+# print("The Number of digits in the number are",count)    
+
+# n=int(input("Enter Number:"))
+# count=0
+# while n>0:
+#     count+=1
+#     n=n//10
+# print("The Number of digits in the number is:",count)    
+
+# n=12345
+# count=0
+# while n>0:
+#     count+=1
+#     n=n//10
+# print("The Number of Digits inn the Sequence is :",count)    
+
+# n=123
+# count=0
+# while n>0:
+#     count+=1
+#     n=n//10
+# print("The Digits in the Number is:",count)    
+
+# n=9876
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Number of digits is:",i)    
+
+# n=764218
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("the Number of digits is:",i)    
+
+# n=123456897
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Number of digits in the Number is: ",i)    
+
+# n=234567
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Number of digits in the Number is:",i)    
+
+# n=8764512
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Number of digits is:",i)    
+
+# n=12345
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("Digits are:",i)    
+
+# n=1234
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Numbber of digits are:",i)
+# # 
+# n=1234567890
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("Digits are:",i)        
+
+# n=int(input("Enter Number:"))
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("The Digits in the Number are:",i)    
+
+# n=9876666666
+# i=0
+# while n>0:
+#     i+=1
+#     n=n//10
+# print("the number of digits are:",i)    
+
+n=87784984928310390249859840249003984898
+i=0
+while n>0:
+    i+=1
+    n=n//10
+print("Number of digits are:",i)    
