d9ee6c93c1b10075e09f836641b59d7c4ad5c815
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-10 17:47:28 +0530

    Armstrong number

---
+++ python_basic35.py
@@ -0,0 +1,200 @@
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+
+#         print(i,"is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i, "is a armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i, "Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#       rem=temp%10
+#       sum+=rem**3
+#       temp//=10
+#     if i==sum:
+#         print(i,"Is the Armstrong Number")
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"is a armstrong number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a armstrong number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armastrong Number")   
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong number")  
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")  
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")         
+
+# for i in range(100,500,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a armstrong number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     c=i
+#     while c>0:
+#         rem=c%10
+#         sum+=c**3
+#         c//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     c=i
+#     while c>0:
+#         rem=c%10
+#         sum+=rem**3
+#         c//=10
+#     if i==sum:
+#         print(i,"Is a armastrong number")    
+
+# for i in range(100,500,1):
+#     sum=0
+#     c=i
+#     while c>0:
+#         rem=c%10
+#         sum+=rem**3
+#         c//=10
+#     if i==sum:
+#         print(i,"armstrong number")    
+
+# for i in range(100,600,1):
+#     sum=0
+#     temp=i
+#     while temp>0:
+#         rem=temp%10
+#         sum+=rem**3
+#         temp//=10
+#     if i==sum:
+#         print(i,"Is a Armstrong Number")    
+
+for i in range(100,500,1):
+    sum=0
+    temp=i
+    while temp>0:
+        rem=temp%10
+        sum+=rem**3
+        temp//=10
+    if i==sum:
+        print(i,"Is a Armstrong Number")    
