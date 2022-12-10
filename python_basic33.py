38033d6c3333775bb71eeb4eed1349471f5cfd6e
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-10 16:10:00 +0530

    Write a program to print inverted star pattern

---
+++ python_basic33.py
@@ -0,0 +1,47 @@
+# n=5
+# for i in range(n,0,-1):
+    # print((n-i)*' '+i*'*')
+
+# n=16
+# for i in range(n,0,-1):
+#     print((n-i)*" "+i*("*"))
+
+# n=20
+# for i in range(n,0,-1):
+# #     print((n-i)*" "+i*"*")
+
+# n=int(input("Enter Number:"))
+# for i in range(n,0,-1):
+#     print((n-i)*" "+i*"* ")
+
+# n=int(input("Enter NUmber:"))
+# for i in range(n,0,-1):
+#     print((n-i)*""+i*"*")
+
+# n=14
+# for i in range(n,0,-1):
+#     print((n-i)*""+i*"*")
+
+# n=9
+# for i in range(n,0,-1):
+#     print((n-i)*""+i*"*")
+
+# n=19
+# for i in range(n,0,-1):
+#     print((n-i)*""+i*"*")
+
+# n=18
+# for i in range(n,0,-1):
+#     print((n-i)*" "+i*("*"))
+
+# n=12
+# for i in range(n,0,-1):
+#     print((n-i)*" "+i*("*"))
+
+# n=17
+# for i in range(n,0,-1):
+#       print((n-i)*""+i*("&"))
+
+n=10
+for i in range(n,0,-1):
+    print((n-i)*""+i*"*")
