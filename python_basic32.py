77d6bb8b9f649bb69cb8338f56eac962b8212634
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-10 15:31:52 +0530

    Write a program to To check entered number is perfect ot not

---
+++ python_basic32.py
@@ -0,0 +1,134 @@
+# n=6
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The Number is perfect") 
+# else:
+#     print("Not perfect")           
+
+# n=7
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect")
+# else:
+#     print("Not Perfect")            
+
+# n=9
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfecto numbrea") 
+# else:
+#     print("No perfecto")          
+
+# n=199
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The Given Number is Perfect")
+# else:
+#     print("The Given Number is not Perfect")         
+
+# n=6
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect Number") 
+# else:
+#     print("Not Perfecto Number")               
+
+# n=14
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The Number is Perfect") 
+# else:
+#     print("The Number is not perfect")           
+
+# n=15
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect Number") 
+# else:
+#     print("Not Perfect Number")           
+
+# n=17
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect Number") 
+# else:
+# #     print("Not perfect number")           
+
+# n=16
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The Number is perfect")
+# else:
+#     print("The Number is not perfect")            
+
+# n=20
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The number is perfect")
+# else:
+#     print("The Number is not perfect")            
+
+# n=18
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect Number")
+# else:
+#     print("Not perfect number")            
+
+# n=int(input("Enter Number:"))
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("The Number is Perfect Number") 
+# else:
+    # print("The NUmber not")  
+
+# n=int(input("Enter Number:"))
+# sum=0
+# for i in range(1,n):
+#     if n%i==0:
+#         sum+=i
+# if sum==n:
+#     print("Perfect Number") 
+# else:
+#     print("No Perfect")                        
+
+n=16
+for i in range(n,0,-1):
+    print((n-1) * ' ' +i* '*')
+
