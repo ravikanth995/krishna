7d4cacd931618a840b2ad268f7bffdc104a9e0f5
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-10 19:21:54 +0530

    Compound Interest

---
+++ python_basic36.py
@@ -0,0 +1,42 @@
+# p=float(input("Enter principal amount:"))
+# r=float(input("Enter Rate of Interest:"))
+# t=float(input("Enter Time Period:"))
+# #Compound Interest
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest:{}".format(ci))
+
+# p=1000
+# r=4.6
+# t=5
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest is:{}".format(ci))
+
+# p,r,t=1500,4.5,5
+# ci=p*(pow((1+r/100),t))
+# print("CI is:{}".format(ci))
+
+# p,r,t=1650,4.87,5.6
+# ci=p*(pow((1+r/100),t))
+# print("Ci is:{}".format(ci))
+
+# p,r,t=1000,4,5
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest is:{}".format(ci))
+
+# p,r,t=100,5,5
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest is:{}".format(ci))
+
+# p,r,t=1000,4,5
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest is:{}".format(ci))
+
+# p,r,t=1500,5,6
+# ci=p*(pow((1+r/100),t))
+# print("Compound Interest is:{}".format(ci))
+
+p=float(input("Enter Principal Amount:"))
+r=float(input("Enter Rate of Interest:"))
+t=float(input("Enter Time Period:"))
+Ci=p*(pow((1+r/100),t))
+print("Compound of Interest Is:{}".format(Ci))
