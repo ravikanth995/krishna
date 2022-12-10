b34d5fa1f2bfad34b5f6a2278182d16a9e09fb6a
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-10 16:28:40 +0530

    Fibonacci correct and accurate

---
+++ python_basic34.py
@@ -0,0 +1,98 @@
+# n=int(input("Enter Number:"))
+# n1=0; n2=1
+# print(n1)
+# print(n2)
+# for i in range(1,n):
+#     n3=n1+n2
+#     print(n3)
+#     n1,n2=n2,n3
+
+# num=8
+# n1=0; n2=1
+# print(n1)
+# print(n2)
+# for i in range(1,num):
+#     n3=n1+n2
+#     print(n3)
+#     n1,n2=n2,n3
+
+# num=9
+# n1=0;n2=1
+# print(n1)
+# print(n2)
+# for i in range(1,num):
+#     n3=n1+n2
+#     print(n3)
+#     n1,n2=n2,n3
+
+# num=19
+# n1=0; n2=1
+# print(n1)
+# print(n2)
+# for i in range(1,num):
+#     c=n1+n2
+#     print(c)
+#     n1,n2=n2,c
+
+# num=12
+# a=0;b=1
+# print(a)
+# print(b)
+# for i in range(1,num):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# num=int(input("Enter Number:"))
+# a,b=0,1
+# print(a)
+# print(b)
+# for i in range(1,num):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# n=int(input("Enter Number:"))
+# a,b=0,1
+# print(a)
+# print(b)
+# for i in range(1,n):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# n=int(input("Enter Number:"))
+# a,b=0,1
+# print(a)
+# print(b)
+# for i in range(1,n):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# n=int(input("Enter Number:"))
+# a,b=0,1
+# print(a)
+# print(b)
+# for i in range(1,n):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+# n=8
+# a,b=0,1
+# print(a)
+# print(b)
+# for i in range(1,n):
+#     c=a+b
+#     print(c)
+#     a,b=b,c
+
+n=6
+a,b=0,1
+print(a)
+print(b)
+for i in range(1,n):
+    c=a+b
+    print(c)
+    a,b=b,c
