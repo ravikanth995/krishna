0f32131db3af993a1ebcf11f7da01a06ad349023
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-11 18:06:27 +0530

    Sort key=len string in-built function

---
+++ python_basic39.py
@@ -0,0 +1,105 @@
+#Ordering Elements of List
+# l=[0,7,5,4,3,2,1,9,8]
+# l.sort()
+# print(l)
+
+# l=[9,8,7,6,5,4,3,2,1,0]
+# l.sort()
+# print(l)
+
+# l=[2,4,1,5,3,6,9,7,8]
+# l.sort()
+# print(l)
+
+# x=[4,5,3,6,7]
+# y=x[:]
+# y.sort()
+# print(y)
+# print(x)
+
+# x=[5,7,3,8]
+# y=x[:]
+# y.sort()
+# print(y)
+
+# x=[1,5,6,3,2]
+# y=x[:]
+# y.sort()
+# print(y)
+
+# x=[1,2,3,6,4,5]
+# y=x[:]
+# y.sort()
+# print(y)
+
+# x=[1,2,3,7,6,5,9]
+# y=x[:]
+# y.sort()
+# print(y)
+
+# x=[1,2,4,5,7]
+# y=x[:]
+# y.sort()
+# print(y)
+
+#If the Python list members are strings, it sorts them accordingly to thier ASCII values
+# y=['Red','yellow','a','A','Orange']
+# y.sort()
+# print(y)
+
+# #Str=sort(key==len)
+# str1=['bbb','aaaaa','ccccccc','kkkkkkkkkk','aa','l']
+# str1.sort(key=len)
+# print(str1)
+
+# str1=['bb','a','ccc','dddd','eeeee']
+# str1.sort(key=len)
+# print(str1)
+
+# str1=['a','bbbb','cc','dddd','tttttt']
+# str1.sort(key=len)
+# print(str1)
+
+# str1=['bb','a','dddd','ccc','eeeee','ffffff']
+# str1.sort(key=len)
+# print(str1)
+
+# str1=['a','eeeee','bb','ccc','dddd']
+# str1.sort(key=len)
+# print(str1)
+
+# l=['bb','a','dddd','ccc','eeeee']
+# l.sort(key=len)
+# print(l)
+
+# l=[1,2,3,4,5,6,7]
+# print("Before reversing",l)
+# l.sort(reverse=True)
+# print("After Reversing the list Elements",l)
+
+# l=[1,2,3,4,5,6,7,8,9]
+# print("Before Reversing the List Elements",l)
+# l.sort(reverse=True)
+# print("After Reversing the List Elements",l)
+
+# l=[1,2,3,4]
+# print("Before Reversing the List Elements:",l)
+# l.sort(reverse=True)
+# print("After Reversing the List Elements:",l)
+
+# l=[1,2,3,4,5,6]
+# print("Before reversing the list elements:",l)
+# l.sort(reverse=True)
+# print("After Reversing the elements:",l)
+
+# l=[1,2,3,4,6]
+# l.sort(reverse=True)
+# print(l)
+
+# l=[1,2,3,4,5,6,7,8]
+# l.sort(reverse=True)
+# print(l)
+
+l=[1,2,3,4,5]
+l.sort(reverse=True)
+print(l)
