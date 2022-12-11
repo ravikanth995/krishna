c2983acc713c816c3af86aa9e124a9e0ed852252
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-11 23:10:19 +0530

    Index and Count in-built functions

---
+++ python_basic40.py
@@ -0,0 +1,13 @@
+#List Accessing Methods
+# #List.index(element[,start[,end]])
+# list1=[1,2,3,1,2,1,2,3,2,1]
+# print(list1.index(1))
+
+# list1=[1,2,2,3,3,4,5,6,7,7,7,7,]
+# print(list1.count(7))
+
+# lst=[1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,5,7,8,9]
+# print(lst.count(3))
+
+lst=[1,1,1,1,1,1,1,1,1,1,3,4,5,6,7,8]
+print(lst.count(1))
