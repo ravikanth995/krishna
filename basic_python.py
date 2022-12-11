b618761140f67cbd5c7fb4cab507584deba78552
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-11 13:48:45 +0530

    Write a program to print all the prime number within given range

--- basic_python.py
+++ basic_python.py
@@ -1,6 +1,206 @@
-n=5
-for i in range(n,0,-1):
-    for j in range(1,i+1):
-        print(i,end=" ")
-    print()    
+# n=int(input("Enter Number:"))
+# for i in range(2,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if (i%j==0):
+#             c=c+1
+#     if c<=0:
+#         print(i,end=" ")        
 
+# n=int(input("Enter Number:"))
+# for i in range(2,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=' ')        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:        
+#         print(i,end=" ")        
+
+# n=12
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=13
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")            
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=91
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#            c+=1
+#     if c<=0:
+#         print(i,end=" ")      
+
+# n=97
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")             
+
+# n=18
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=19
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=29
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")            
+
+# n=31
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")            
+
+# n=9
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=" ")        
+
+# n=int(input("Enter Number:"))
+# for i in range(1,n+1):
+#     c=0
+#     for j in range(2,i//2+1):
+#         if i%j==0:
+#             c+=1
+#     if c<=0:
+#         print(i,end=' ')        
+
+n=int(input("Enter Number:"))
+for i in range(1,n+1):
+    c=0
+    for j in range(2,i//2+1):
+        if i%j==0:
+            c+=1
+    if c<=0:
+        print(i,end=" ")        
