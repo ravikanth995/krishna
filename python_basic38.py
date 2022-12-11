9cd5ba76a4593e8559f8bbd43dc2ce936de1fb6b
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-11 17:20:54 +0530

    remove pop del built-in functions

---
+++ python_basic38.py
@@ -0,0 +1,92 @@
+#Remove and Pop built-in Functions
+# lst=[20,30,40]
+# lst.remove(30)
+# print(lst)
+
+# lst=[10,20,30,40]
+# lst.remove(20)
+# print(lst)
+
+# lst=[2,8,6,4]
+# lst.remove(2)
+# # print(lst)
+
+# lst=[1,2,3,4,5]
+# lst.pop(1)
+# print(lst)
+
+# lst.pop()
+# print(lst)
+
+# lst.pop()
+# print(lst)
+
+# lst.pop()
+# print(lst)
+
+# lst.pop()
+# print(lst)
+
+# lst.pop()
+# print(lst)
+
+# lst=[1,2,3,4,5,6,7,8,9]
+# lst.clear()
+# print(lst)
+
+# lst=[1,2,3,4,5,6,7,8,9]
+# lst.clear()
+# print(lst)
+
+# l=['ravi','kanth','upanishad','vedant','satya','isavasya']
+# del l[0:5]
+# print(l)
+
+# l=['Satya','Shakti','Astya','Asatya']
+# del l[1]
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# del l[0]
+# print(l)
+
+l=[1,2,3,4]
+# l.remove(1)
+# print(l)
+
+# l.remove(2)
+# print(l)
+
+# l.pop()
+# print(l)
+
+# del l[1]
+# print(l)
+
+# l=[1,2,3,4,5]
+# del l[0:3]
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# del l[0:4]
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# del l[1:4]
+# print(l)
+
+# l=['ravi','kanth','buddha','krishna','Rama','Ashtavkra']
+# del l[1:3]
+# print(l)
+
+# l=['ravi','kanth','Buddha','chanakya','Kautilya','Krishna']
+# del l[0:1]
+# print(l)
+
+# l=[1,2,3,4,5,6,7]
+# del l[0:4]
+# print(l)
+
+l=[0,1,2,3,4,5,6,7,8]
+del l[1:3]
+print(l)
