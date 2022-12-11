e1ca09a6ce9c0b13072733dbfd02528110389d18
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-11 15:57:38 +0530

    List append

---
+++ python_basic37.py
@@ -0,0 +1,117 @@
+# # #Adding Elements in the List
+# # list=[]
+# # print(list)
+# # list.append(10)
+# # list.append(20)
+# # list.append(30)
+# # print(list)
+
+# # list=[]
+# # for i in range(10,20):
+# #     list.append(i)
+# # print("After Addition of Elements from 10-19:") 
+# # print(list)   
+
+# # list=[]
+# # for i in range(10,20):
+# #     list.append(i)
+# # print("After Appending")
+# # print(list)
+
+# # list=[]
+# # for i in range(10,20):
+# #     list.append(i)
+# # print(list)
+
+# # lst=[]
+# # for i in range(10,20):
+# #     lst.append(i)
+# # print(lst)    
+
+# # lst=[]
+# # for i in range(10,30):
+# #     lst.append(i)
+# # print(lst)    
+
+# # lst=[]
+# # for i in range(10,15):
+# #     lst.append(i)
+# # print(lst)  
+
+# lst=[]
+# for i in range(10,18):
+#     lst.append(i)
+# print(lst)
+
+# l=[]
+# for i in range(0,20):
+#     l.append(i)
+# print(l)    
+
+# l=[]
+# for i in range(0,12):
+#     l.append(i)
+# print(l)    
+
+# l=[]
+# for i in range(1,4):
+#     l.append(i)
+# print(l)    
+
+# b=['apple','orange']
+# b.append(l)
+# print(b)
+
+# l=[]
+# for i in range(0,7):
+#     l.append(i)
+# print(l)
+
+# b=['ravi','kanth']
+# b.append(l)
+# print(b)
+
+# l=[]
+# for i in range(0,8):
+#     l.append(i)
+# print(l)
+
+# b=['Ravi','kanth']
+# b.append(l)
+# print(b)
+
+# l=[]
+# for i in range(0,18):
+#     l.append(i)
+# print(l)
+
+# b=[1,2]
+# b.append(l)
+# print(b)
+
+# l=[]
+# for i in range(0,7):
+#     l.append(i)
+# print(l)
+
+# b=['ravi','kanth']
+# b.append(l)
+# print(b)
+
+# l=[]
+# for i in range(0,11):
+#     l.append(i)
+# print(l)
+
+# b=[0,1,2,3]
+# b.append(l)
+# print(b)
+
+l=[]
+for i in range(0,19,2):
+    l.append(i)
+print(l)
+
+b=[9,7,6]
+b.append(l)
+print(b)
