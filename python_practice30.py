698ab652a74779dd97ddc4dc5c025f0d15669420
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-16 16:33:18 +0530

    Write a program which takes list of numbers as input and finds 1. The Largest number in the List, 2. Smallest number and 3. product of all the elements in the List

--- python_practice30.py
+++ python_practice30.py
@@ -79,11 +79,111 @@
 # print("Minimum in the List:",min)
 # print("product of the List:",total)
 
+# list=[]
+# n=int(input("Enter Number of Size:")) 
+# print("\n")
+# for i in range(0,n):
+#     print("Enter number location:",i,":")
+#     item=int(input())
+#     list.append(item)
+# total=1
+# max=list[0]
+# min=list[0]
+# for a in list:
+#     total*=a
+#     if a>max:
+#         max=a
+#     if a<max:
+#         min=a
+# print("Maximum element in the List:",max)
+# print("Minimum element in the List:",min)
+# print("product of the Element:",total)                         
+
+# list=[]
+# n=int(input("Enter Size of the List:"))
+# print("\n")
+# for i in range(0,n):
+#     print("Enter Number at Location:",i,":")
+#     item=int(input())
+#     list.append(item)
+# max=list[0]
+# min=list[0]
+# total=1
+# for a in list:
+#     total*=a
+#     if a>max:
+#         max=a
+#     if a<min:
+#         min=a
+# print("Maximum Element in the List:",max)
+# print("Minimum Element in the List:",min)
+# print("product of the Element in the list:",total)             
+
+# list=[]
+# n=int(input("Enter Size of the List:"))
+# print("\n")
+# for i in range(0,n):
+#     print("Enter Number at Location:",i,":")
+#     item=int(input())
+#     list.append(item)
+# total=1
+# max=list[0]
+# min=list[0]
+# for a in list:
+#     total*=a
+#     if a>max:
+#         max=a
+#     if a<min:
+#         min=a
+# print("Maximum Number of Element in the List:",max)
+# print("Minimum Number of Elements in the List is:",min)
+# print("product of the List is:",total)                   
+
+# list=[]
+# n=int(input("Enter Size of List:"))
+# print("\n")
+# for i in range(0,n):
+#     print("Enter Number at Location",i,":")
+#     item=int(input())
+#     list.append(item)
+# total=1
+# max=list[0]
+# min=list[0]
+# for a in list:
+#     total*=a
+#     if a>max:
+#         max=a
+#     if a<min:
+#         min=a
+# print("Maximum Element in the List:",max)
+# print("Minimum Element in the List:",min)
+# print("product of list:",total)                
+
+# list=[]
+# n=int(input("Enter Size of the List:"))
+# print("\n")
+# for i in range(0,n):
+#     print("Enter Number at Location:",i,":")
+#     item=int(input())
+#     list.append(item)
+# total=1
+# max=list[0]
+# min=list[0]
+# for a in list:
+#     total*=a
+#     if a>max:
+#         max=a
+#     if a<min:
+#         min=a
+# print("Maximum Element in the List:",max)
+# print("Minimum Element in the List:",min) 
+# print("product of list:",total)               
+
 list=[]
-n=int(input("Enter Number of Size:")) 
+n=int(input("Enter Size of List:"))
 print("\n")
 for i in range(0,n):
-    print("Enter number location:",i,":")
+    print("Enter Numnber at Location:",i,":")
     item=int(input())
     list.append(item)
 total=1
@@ -93,8 +193,8 @@ for a in list:
     total*=a
     if a>max:
         max=a
-    if a<max:
+    if a<min:
         min=a
-print("Maximum element in the List:",max)
-print("Minimum element in the List:",min)
-print("product of the Element:",total)                         
+print("Maximum Element in the List:",max)
+print("Minimum Element in the List:",min)
+print("product of the list Element:",total)                
