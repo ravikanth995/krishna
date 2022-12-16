4d81f5b94ca7dbb2bdeab6cfffc722bc1dfd98dd
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-16 15:09:17 +0530

    Linear and Binary Search

---
+++ python_basic45.py
@@ -0,0 +1,307 @@
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+
+# #driver Code
+# arr=[10,20,30,40]
+# s=20
+# n=len(arr)
+
+# #Function call
+# result=search(arr,n,s)
+# if result==-1:
+#     print("Element is not present in the array")
+# else:
+#     print("Elements",s, " is present in the index:",result)    
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+
+# #driver code
+# arr=[10,12,14,23,35]
+# s=12
+# n=len(arr)
+
+# #function call
+# result=search(arr,n,s)
+# if result==-1:
+#     print("The Element not found")
+# else:
+#     print("Element",s,"is present in the index:",result)    
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[10,20,30,40,50,60]
+# n=len(arr)
+# s=40
+
+# result=search(arr,n,s)
+# if result==-1:
+#     print("The Elements are not found:")
+# else:
+#     print("Element",s,"Is presenting in the index:-->",result)    
+
+# def Search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[10,20,30,60]
+# n=len(arr)
+# s=60
+
+# result=Search(arr,n,s)
+# if result==-1:
+#     print("The Element is not presnting the array")
+# else:
+#     print("Element",s,"is present in the index:",result)    
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[10,20,30,40,50,60]
+# s=50
+# n=len(arr)
+
+# result=search(arr,n,s)
+# if result==-1:
+#     print("The Elements are not present")
+# else:
+#     print("The Element",s,"is present in inndex -->",result)    
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[20,28,27,25,70] 
+# s=27
+# n=len(arr) 
+# result=search(arr,n,s) 
+# if result==-1:
+#     print("the elements are not present in array")
+# else:
+#     print("Element",s,"is present in the index-->",result)    
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[1,2,3,4,5,6] 
+# n=len(arr) 
+# s=4 
+# result=search(arr,n,s)
+# if result==-1:
+#     print("Elements are not present in the array")
+# else:
+#     print("Element",s,"is present in the index:",result)  
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[1,2,3,4,5,6]
+# s=6
+# n=len(arr)
+# result=search(arr,n,s)
+# if result==-1:
+#     print("The Elements are not present in the array")
+# else:
+#     print("The elements",s,"present in the index:",result) 
+
+# def search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[1,2,3,4,5,6]
+# n=len(arr)
+# s=6 
+# result=search(arr,n,s)
+# if result==-1:
+#     print("The elements not found in array")
+# else:
+#     print("Element",s,"is present in the index-->",result)   
+
+# def Search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[0,1,2,3,4,5,6,7] 
+# n=len(arr)
+# s=7
+# a=Search(arr,n,s)
+# if a==-1:
+#     print("Element not found")
+# else:
+#     print("Element",s,"is found in index:",a)                               
+
+# def Search(arr,n ,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[4,1,2,6,8,3,9]
+# n=len(arr)
+# s=8
+# result=Search(arr,n,s)
+# if result==-1:
+#     print("element not found")
+# else:
+#     print("The element",s,"is found in index:",result)  
+
+# def Search(arr,n,s):
+#     for i in range(0,n):
+#         if arr[i]==s:
+#             return i
+#     return -1
+# arr=[0,4,1,3,8,5,2]
+# n=len(arr)
+# s=2
+# result=Search(arr,n,s)
+# if result==-1:
+#     print("Elements not found in Array")
+# else:
+#     print("Element",s,"is present in index",result)                                      
+
+#Linear search on tuple
+# def check(tuple, n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(10,20,30,40,50,60)
+# n=int(input("Enter a Number:"))
+# if check(tuple,n):
+#     print("found")
+# else:
+#     print("not found")                   
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5,6) 
+# n=int(input("Enter a number"))
+# if check(tuple,n):
+#     print("found")
+# else:
+#     print("found")     
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5)
+# n=4
+# if check(tuple,n):
+#     print("found")
+# else:
+#     print("not found")                
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5,6)
+# n=int(input("Enter  number:"))
+# if check(tuple,n):
+#     print("Found")
+# else:
+#     print("Not Found")                
+  
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5,6)
+# n=4
+# if check(tuple,n):
+#     print("Found")
+# else:
+#     print("not found")                    
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5,6,7) 
+# n=5
+# if check(tuple,n):
+#     print("Found")
+# else:
+#     print("Not Found")        
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5) 
+# n=int(input("Enter a Number:")) 
+# if check(tuple,n):
+#     print("Found")
+# else:
+#     print("Not Found")                     
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4,5,6)
+# n=6
+# if check(tuple,n) :
+#     print("Found")
+# else:
+#     print("Not Found")               
+
+# def check(tuple,n):
+#     for i in range(len(tuple)):
+#         if tuple[i]==n:
+#             return True
+#     return False
+# tuple=(1,2,3,4) 
+# n=3
+# if check(tuple,n):
+#     print("Found") 
+# else:
+# print("Not Found")                 
+
+# dict={1:2, 3:4, 5:6, 7:8}
+# # for i in dict.items():
+# #     print(i)
+# for i in dict.items():
+#        print(i)
+
+def count(mylist):
+    frequency={}
+    for i in mylist:
+        if (i in frequency):
+            frequency[i]+=1
+        else:
+            frequency[i]=1
+    for key, values in frequency.items():
+        print("%d : %d"%(key,values))
+mylist=[5,8,3,4,0,1,2,3] 
+count(mylist)               
+
+
