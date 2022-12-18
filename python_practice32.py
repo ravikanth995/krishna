b1b37d3834d93e65edbdbbd7ed8ee970aef1f2f9
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-18 11:43:16 +0530

    two matrices and sum of even and odd

---
+++ python_practice32.py
@@ -0,0 +1,346 @@
+# list1=[[20,71],
+# [41,51],
+# [33,81]]
+
+# transpose=[[0,0,0],
+#          [0,0,0]]
+
+# #iterate through rows
+# for i in range(len(list1)):
+#     #iterate through columns
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#    print(r)                 
+
+# list1=[[20,71],
+#       [41,51],
+#       [33,41]]
+# transpose=[[0,0,0],
+#         [0,0,0]] 
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)    
+
+# list1=[[31,81],
+#      [21,31],
+#      [91,73]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j] 
+# for r in transpose:
+#     print(r)                                      
+
+# list1=[[21,22],
+#       [31,32],
+#       [41,42]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                  
+
+# list1=[[1,2],
+#      [3,4],
+#      [6,7]]
+# transpose=[[0,0,0],
+#          [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)              
+
+# list1=[[1,2],
+#       [3,4],
+#       [5,6]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)
+
+# list1=[[1,2],
+#        [7,5],
+#        [9,1]]
+# transpose=[[0,0,0],
+#            [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                          
+
+# list1=[[3,2],
+#       [4,5],
+#       [9,5]]
+# transpose=[[0,0,0],
+#          [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):        
+#            transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)        
+
+# list1=[[1,2],
+#      [3,4],
+#      [5,6]]
+# transpose=[[0,0,0],
+#          [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                      
+
+# list1=[[3,1],
+#       [4,5],
+#       [6,3]]
+# transpose=[[0,0,0],
+#            [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])) :
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                        
+
+# list1=[[1,2],
+#      [5,6],
+#      [9,5]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j] 
+# for r in transpose:
+#     print(r)                      
+
+# list1=[[2,3],
+#        [6,5],
+#        [6,7]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                         
+
+# list1=[[5,4],
+#      [7,8],
+#      [5,5]]
+# transpose=[[0,0,0],
+#           [0,0,0]] 
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j] 
+# for r in transpose:
+#     print(r)                     
+
+# list1=[[1,4],
+#       [5,6],
+#      [8,1]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                      
+
+# list1=[[1,3],
+#      [4,5],
+#      [6,6]]
+# transpose=[[0,0,0],
+#          [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                      
+
+# list1=[[0,1],
+#       [2,3],
+#       [4,5]]
+# transpose=[[0,0,0],
+#           [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         transpose[j][i]=list1[i][j]
+# for r in transpose:
+#     print(r)                            
+
+#Two Matrices loop
+# m1=[[1,2,3],
+#     [4,5,6],
+#     [7,8,9]]
+# m2=[[1,2,3],
+#    [4,5,6],
+#   [7,8,9]]
+# add=[[0,0,0],
+#    [0,0,0],
+#    [0,0,0]] 
+# for i in range(len(m1)):
+#     for j in range(len(m2[0])):
+#         add[i][j]=m1[i][j]+m1[i][j]
+# for r in add:
+#     print(r)               
+
+# list1=[[1,3,2],
+#       [4,6,4],
+#        [9,8,6]]
+# list2=[[6,3,5],
+#       [9,2,1],
+#        [3,4,5]] 
+# add=[[0,0,0],
+#     [0,0,0],
+#     [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[i])):
+#         add[i][j]=list1[i][j]+list2[i][j]
+# for r in add:
+#     print(r)        
+
+# list1=[[3,2,1],
+#      [1,2,3],
+#      [4,3,2]]
+# list2=[[3,4,5],
+#     [6,7,8],
+#     [9,4,1]] 
+# add=[[0,0,0],
+#    [0,0,0],
+#    [0,0,0]]
+# for i in range(len(list1)):
+#     for j in range(len(list1[0])):
+#         add[i][j]=list1[i][j]+list2[i][j]
+# for r in add:
+#     print(r)                  
+
+# m1=[[3,2,4],
+#     [6,7,5],
+#     [9,1,4]]
+# m2=[[1,2,3],
+#    [3,4,5],
+#    [8,5,2]]
+# add=[[0,0,0],
+#    [0,0,0],
+#    [0,0,0]]
+# for i in range(len(m1)):
+#     for j in range(len(m2[0])):
+#         add[i][j]=m1[i][j]+m2[i][j]
+# for r in add:
+#     print(r)           
+
+#Find the Sum of Even and odd numbers in a list'
+# num=[]
+# E_sum=0
+# O_sum=0
+# n=int(input("Enter Number of List:"))
+# for i in range(1,n+1):
+#     value=int(input("Enter Element:"))
+#     num.append(value)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_sum=E_sum+num[j]
+#     else:
+#         O_sum=O_sum+num[j]        
+# print("Sum of Even Numbers in the List:",E_sum)
+# print("Sum of odd Number in list:",O_sum)        
+
+# num=[]
+# E_sum=0
+# O_sum=0
+# n=int(input("Enter NUmber of Elements in a list:"))
+# for i in range(1,n+1):
+#     value=int(input("Enter Number:"))
+#     num.append(value)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_sum=E_sum+num[j]
+#     else:
+#         O_sum=O_sum+num[j]
+# print("Sum of Even Number is:",E_sum) 
+# print("Sum of od sum number is:",O_sum)           
+
+# num=[]
+# E_sum=0
+# O_sum=0
+# n=int(input("Enter NUmber of Elements in a List:"))
+# for i in range(1,n+1):
+#     values=int(input("Enter Number:"))
+#     num.append(values)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_sum=E_sum+num[j]
+#     else:
+#         O_sum=O_sum+num[j]
+# print("Sum of Even Number:",E_sum)
+# print("Sum of Odd Number:",O_sum)  
+
+# num=[]
+# E_Sum=0
+# O_Sum=0
+# n=int(input("Enter Number of Element in a List:"))
+# for i in range(1,n+1):
+#     values=int(input("Enter Numbers:"))              
+#     num.append(values)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_Sum=E_Sum+num[j]
+#     else:
+#         O_Sum=O_Sum+num[j]
+# print("Sum of Even Number is:",E_Sum) 
+# print("Sum of Odd Number:",O_Sum)           
+
+# num=[]
+# E_sum=0
+# O_sum=0
+# n=int(input("Enter Number of Elements in a List:"))
+# for i in range(1,n+1):
+#     values=int(input("Enter Number:"))
+#     num.append(values)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_sum=E_sum+num[j]
+#     else:
+#         O_sum=O_sum+num[j]
+# print("Sum of Even Number is:",E_sum)
+# print("Sum of Odd Number is:",O_sum)          
+
+# num=[]
+# E_sum=0
+# O_sum=0
+# n=int(input("Enter Number of Elements in a List:"))
+# for i in range(1,n+1):
+#     values=int(input("Enter Number:"))
+#     num.append(values)
+# for j in range(n):
+#     if num[j]%2==0:
+#         E_sum=E_sum+num[j]
+#     else:
+#         O_sum=O_sum+num[j]
+# print("Sum of Even Is:",E_sum)
+# print("Sum of Odd is:",O_sum)            
+
+num=[]
+E_sum=0
+O_sum=0
+n=int(input("Enter Number of Elements in a List:"))
+for i in range(1,n+1):
+    values=int(input("Enter Number:"))
+    num.append(values)
+for j in range(n):
+    if num[j]%2==0:
+        E_sum=E_sum+num[j] 
+    else:
+        O_sum=O_sum+num[j]
+print("Even Sum is:",E_sum)
+print("Odd Sum is:",O_sum)               
