057dcb6d710952d0bc5b82256e3a3c6e0f37c421
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-30 12:26:49 +0530

    Command Line Argument

---
+++ python_basic52.py
@@ -0,0 +1,53 @@
+#Command Line Arguments(very Important)
+# #there is a another method rather input() for user provided data
+#argv is a list type variable present in thr sys module
+
+# from sys import argv
+# print(type(argv))
+
+
+#Write a program to display number of command line argument or length of command line arguments
+# from sys import argv
+# print("The No. of Command Line Argument:",len(argv))
+# print("The List of Command Line Argument:",argv)
+# print("Command Line Argument are:..")
+# for i in argv:
+#     print(i)
+
+# from sys import argv
+# sum=0
+# args=argv[1:]
+# for i in args:
+#     sum=sum+int(i)
+# print(sum)    
+
+# from sys import argv
+# print(argv[1])
+
+# from sys import argv
+# print(argv[1]+argv[2])
+# print(int(argv[1])+int
+# (argv[2]))
+
+# from sys import argv
+# print(argv[10])
+
+# from sys import argv
+# print(len(argv))
+# print(argv)
+# for i in argv:
+#     print(i)
+
+# #For string
+# from sys import argv
+# print(argv[1])
+
+# from sys import argv
+# print(len(argv))
+
+# from sys import argv
+# print(argv)
+
+from sys import argv
+print(argv[1]+argv[2])
+print((int(argv[1]))+int(argv[2]))
