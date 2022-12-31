f6a8b3b567f4cab85222c70d2fd4fa91784f844d
ravikanth995 <ravikanthchauhan95@gmail.com>
2022-12-31 12:27:59 +0530

    File Handling Problems including file copy to another file

---
+++ python_basic53.py
@@ -0,0 +1,329 @@
+#write a python program to write list content to a file
+# language=['python','java','.Net','C','C++']
+# with open("buddha.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open("buddha.txt")
+# print(content.read())        
+
+# language= ['Python','Core Java','.Net','C','C++']
+# with open("Buddha1.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open("buddha1.txt")
+# print(content.read())        
+
+# language=['Python','Core Java','C','C++']
+# with open("Buddha2.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open("Buddha2.txt")
+# print(content.read())        
+
+# language=['C','C++','Core Java','Python']
+# with open("buddha2.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open("buddha2.txt")
+# print(content.read())        
+
+# language=['C','C++','Core Java','Python']
+# with open("buddha3.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open("buddha3.txt")
+# print(content.read())        
+
+# langauge=['Ravikanth','Harisingh','Omla','Chavan']
+# with open("buddha4.txt",'w') as file:
+#     for l in langauge:
+#         file.write("%s\n"%l)
+# content=open("buddha4.txt")
+# print(content.read())        
+
+# language=['C','C++','Core Java','Python']
+# with open("buddha5.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# content=open('buddha5.txt')
+# print(content.read())      
+
+# langauge=['C','C++','Core Java','Python','.Net','JPython'] 
+# with open("buddhaa.txt",'w') as file:
+#     for l in langauge:
+#         file.write("%s\n"%l)
+# content=open("buddhaa.txt")
+# print(content.read())         
+
+# langauge=['Vedic','Ashtavakra','Buddha','Mahaveera','ShankarAcharya','Kabir','Madhavacharya','Paramhans','Vivekanand','Raman Maharshi','Osho','JKrishnamurthi','Acharya Prashant']
+# with open("myfile.txt",'w') as file:
+#     for l in langauge:
+#         file.write("%s\n"%l)
+# content=open("myFile.txt")
+# print(content.read())        
+
+# language=['Sravam','Dukham','Dukkha','Nirodh']
+# with open("MyText.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# okay=open("MyText.txt")
+# print(okay.read())        
+
+# language=['OSho','Punya','Kya','hai','Sukh']
+# with open("sukh.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# my=open("sukh.txt")
+# print(my.read())        
+
+# language=['Assert','Negate','Assessment','Visualize','Analyse','Criticise','critical','thinking']
+# with open("mukht.txt",'w') as file:
+#     for l in language:
+#         file.write("%s\n"%l)
+# fun=open("mukht.txt") 
+# print(fun.read())      
+
+# langauge=['Modern','Man','Ancient','Man','SuperModern','UltraModern Man']
+# with open("open.txt",'w')as file:
+#     for l in langauge:
+#         file.write("%s\n"%l)
+# ok=open("open.txt")
+# print(ok.read())         
+
+# lang=['Sense Organs','Sense Objects','Mind','Intellect','Atman']
+# with open("Atma.txt",'w') as file:
+#     for l in lang:
+#         file.write("%s\n"%l)
+# ok=open("Atma.txt")
+# print(ok.read())        
+
+# lang=['Veda','Samkhya Darshan','Upanishads','Dhammadapada','Bhagavati Sutra','Kabir dohe','Bhakti sutra']
+# with open("veda.txt",'w')as file:
+#     for l in lang:
+#         file.write("%s\n"%l)
+# ok=open("veda.txt")
+# print(ok.read())        
+
+# lang=['Vichar','Vimarsh','Karo','Samajh','Se','Chalo']
+# with open("mytext.txt",'w')as file:
+#     for l in lang:
+#         file.write("%s\n"%l)
+# pk=open("mytext.txt")
+# print(pk.read())        
+
+#Write Program to read a random line from a file
+# import random
+# def random_line(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_line('write.txt'))    
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))    
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))    
+
+# import random
+# def random_line(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_line('write.txt'))    
+
+# import random
+# def random_no(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_no('write.txt'))    
+
+# import random
+# def random_line(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_line('write.txt'))    
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))    
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))    
+
+# import random
+# def random_lines_from_file(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines_from_file('write.txt'))    
+
+# import random
+# def print_random_line_from_file(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(print_random_line_from_file('write.txt'))    
+
+# import random
+# def random_lines_display_from_file(file_name):
+#     lines=open(file_name).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines_display_from_file('write.txt'))    
+
+# import random
+# def random_number_display_from_file(file_name):
+#     lines=open(file_name).read().splitlines()
+#     return random.choice(lines)
+# print(random_number_display_from_file('write.txt'))
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))        
+
+# import random
+# def random_lines(fname):
+#     lines=open(fname).read().splitlines()
+#     return random.choice(lines)
+# print(random_lines('write.txt'))
+
+#Write a program to assess if a file closed or not
+# f=open('write.txt','w')
+# print(f.closed)
+# f.close()
+# print(f.closed)
+
+# f=open('write.txt','w')
+# print(f.closed)
+# f.close()
+# print(f.closed)
+
+#Write a program to read file word by word
+# with open('ravi.txt','r') as file:
+#     #read each line by line
+#     for line in file:
+#         #reading each word
+#         for word in line.split():
+#             #displaying the words
+#             print(word)
+
+# with open('ravi1.txt','r') as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi3.txt','r')as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi5.txt','r') as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi3.txt','r')as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi2.txt','r') as file:
+#     for lines in file:
+#         for word in lines.split():
+#             print(word)
+
+# with open('ravi1.txt','r') as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi.txt','r')as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi3.txt','r')as file:
+#     for line in file:
+#         for word in line.split():
+#             print(word)
+
+# with open('ravi.txt','r')as file:
+#     for i in file:
+#         for word in i.split():
+#             print(word)
+
+#Write a program to copy contents of the first file into the second file
+#open both files
+# file1=open('ravi.txt','r')
+# file2=open('ravi1.txt','a')
+
+# #read content from the first file
+# for line in file1:
+#     #write content on second file or copying each line to new file
+#     file2.write(line)
+# print("File Copied Successfully")
+# file1.close()
+# file2.close()    
+
+# f1=open('ravi.txt','r')
+# f2=open('file12.txt','a')
+# for line in f1:
+#         f2.write(line)
+# print("Copied Successfully")
+# f1.close()        
+# f2.close()
+
+# f1=open('ravi3.txt','r')
+# f2=open('file11.txt','a')
+# for line in f1:
+#     f2.write(line)
+# print("Copied Successfully")
+# f1.close()
+# f2.close()    
+
+# f1=open('ravi.txt','r')
+# f2=open('new_file.txt','a')
+# for line in f1:
+#     f2.write(line)
+# f1.close()
+# f2.close()    
+
+# f1=open("ravi4.txt",'r')
+# f2=open('z.txt','a')
+# for i in f1:
+#     f2.write(i)
+# f1.close()
+# f2.close()    
+
+# f1=open('ravi5.txt','r')
+# f2=open('z1.txt','a')
+# for i in f1:
+#     f2.write(i)
+# print("Copied Successfully") 
+# f1.close()
+# f2.close()   
+
+# f1=open('ravi3.txt','r')
+# f2=open('z11.txt','a')
+# for i in f1:
+#     f2.write(i)
+# print("Copied Successfully")    
+# f1.close()
+# f2.close()   
+
+f1=open('ravi1.txt','r')
+f2=open('z22.txt','a')
+for i in f1:
+    f2.write(i)
+print("Copied Successfully")
+f1.close()
+f2.close()     
