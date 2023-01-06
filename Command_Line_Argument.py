7f43ce788b8e937b1eb3139bc3b55b8a57a7c127
ravikanth995 <ravikanthchauhan95@gmail.com>
2023-01-06 21:08:00 +0530

    Command Line Argument

--- Command_Line_Argument.py
+++ Command_Line_Argument.py
@@ -47,14 +47,79 @@
 #     sum+=int(sys.argv[i])
 # print("\n\nSum is:",sum) 
 
-import sys
-n=len(sys.argv) 
-print("\nNumber of Argument Passed:",n)
-print("\nName of the Argument is:",sys.argv[0])
-print("\nArguments Passed are:",end=" ")
-for i in range(1,n):
-    print(sys.argv[i],end=" ")
-sum=0
-for i in range(1,n):
-    sum+=int(sys.argv[i])
-print("\nResult is:",sum)              
+# import sys
+# n=len(sys.argv) 
+# print("\nNumber of Argument Passed:",n)
+# print("\nName of the Argument is:",sys.argv[0])
+# print("\nArguments Passed are:",end=" ")
+# for i in range(1,n):
+#     print(sys.argv[i],end=" ")
+# sum=0
+# for i in range(1,n):
+#     sum+=int(sys.argv[i])
+# print("\nResult is:",sum)    
+
+# import sys
+# import os
+# n=len(sys.argv)
+# print("Nuumber of Argument Passed:",n)
+# print("Name of the Argument is:",sys.argv[0])
+# print("Argument passed:")
+# for i in range(1,n):
+#     print(sys.argv[i],end=" ")
+# sum=0
+# for i in range(1,n):
+#     sum+=int(sys.argv[i])
+# print("Sum is:",sum)           
+
+# import sys
+# n=len(sys.argv)
+# print("\nNumber of Argument Passed:",n) 
+# print("\nName of the Argument is:",sys.argv[0])
+# print("Argument Passed:",end=" ")
+# for i in range(1,n):
+#     print(sys.argv[i],end=" ")
+# sum=0
+# for  i in range(1,n):
+#     sum+=int(sys.argv[i])
+# print("\nResult is:",sum)     
+
+# import sys
+# n=len(sys.argv)
+# print("\nNumber of Argument is:",n)
+# print("\nName of the Argument is:",sys.argv[0])
+# print("\nArguments are:",end=" ") 
+# for i in range(1,n):
+#     print(sys.argv[i],end=" ")
+# sum=0
+# for i in range(1,n):
+#     sum+=int(sys.argv[i])
+# print("\n\nResult is:",sum)        
+
+# import sys
+# n=len(sys.argv)
+# print("\nNumber of Arguments are:",n)
+# print("\nName of the Argument is:",sys.argv[0])
+# print("\nArguments are:",end=" ")
+# for i in range(1,n):
+#     print(sys.argv[i],end=" ")
+# sum=0
+# for i in range(1,n):
+#     sum+=int(sys.argv[i])
+# print("\nSum is:",sum)        
+
+import getopt, sys
+argumentList=sys.argv[1:]
+options="hmo:"
+long_options=["Help","my_file","output="]
+try:
+    argument, values=getopt.getopt(argumentList, options, long_options)
+    for currentArgument, currentValue in argument:
+        if currentArgument in ("-h","__help"):
+           print("Displaying help")
+        elif currentArgument in ("-m","__help"):
+           print("Displaying file name",sys.argv[0]) 
+        elif currentArgument in ("-o","__output"):
+           print("Enabling Special Output mode(%s)")%currentValue
+except getopt.error as err:
+    print(str(err))
