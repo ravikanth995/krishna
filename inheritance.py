e97970a4a955bf59fcb8a52e2a0347761814bd79
ravikanth995 <ravikanthchauhan95@gmail.com>
2023-01-16 12:23:27 +0530

    Inheritance

--- inheritance.py
+++ inheritance.py
@@ -79,12 +79,69 @@
 # test.DoC()
 # test.Class()          
 
-class Science:
+# class Science:
+#     def func(self):
+#         print("Hi World")
+# class Maths(Science):
+#     def func1(self):
+#         print("Hi SciFi")
+# test=Maths()
+# test.func()
+# test.func1()                  
+
+# #Multi-ple Inheritance
+# class GrandFather:
+#     def func(self):
+#         print("GrandFather India")
+# class Father:
+#     def func1(self):
+#         print("Hello.. Father")
+# class Mother:
+#     def func2(self):
+#         print("Mother India")
+# class Child(GrandFather,Father,Mother):
+#     def func3(self):
+#         print("Hi... Child")
+# test=Child()
+# test.func()
+# test.func1()
+# test.func2()
+# test.func3()
+# print(Child.__mro__)                
+
+# class Indus_Valley:
+#     def func(self):
+#         print("Harappan Valley")
+# class Vedic_Period:
+#     def func1(self):
+#         print("Vedic Age") 
+# class Later_Vedic:
+#     def func2(self) :
+#         print("Later Vedic Period")
+# class Present_Time(Indus_Valley,Vedic_Period,Later_Vedic):
+#     def func3(self):
+#         print("At Present is 2023")
+# test=Present_Time()
+# test.func()
+# test.func1()
+# test.func2()
+# test.func3()  
+# print(Present_Time.__mro__)              
+
+class Hinduism:
     def func(self):
-        print("Hi World")
-class Maths(Science):
+        print("Hinduism is the Mother")
+class Buddhism:
     def func1(self):
-        print("Hi SciFi")
-test=Maths()
+        print("Buddha is reformer")
+class Jainism:
+    def func2(self):
+        print("Mahaveer Vardhaman is peaceful")
+class Logic(Hinduism,Buddhism,Jainism):
+    def func3(self):
+        print("Logic is evolved from Dharmic religions")
+test=Logic()
 test.func()
-test.func1()                                
+test.func1()
+test.func2()
+test.func3()                
