72d121173d9bd94734ca6728156bc51d3f8e0333
ravikanth995 <ravikanthchauhan95@gmail.com>
2023-01-20 11:20:12 +0530

    Multilevel Inheritance

--- multilevel_inheritance.py
+++ multilevel_inheritance.py
@@ -79,28 +79,48 @@
 # Obj=GrandChild_class("Ravikanth",27,"Chittapur")
 # print(Obj.get_name(),Obj.get_Age(),Obj.get_Location())                                    
 
-class Parent_class:
-    def __init__(self,name):
-        self.name=name
-    def get_name(self):
-        return self.name
-class Child_class(Parent_class):
-    def __init__(self,name,age):
-        Parent_class.__init__(self,name)
-        self.age=age
-    def get_age(self):
-        return self.age
-class GrandChild_class(Child_class):
-    def __init__(self,name,age,location):
-        Child_class.__init__(self,name,age)
-        self.location=location
-    def get_Location(self):
-        return self.location
-Obj1=GrandChild_class("RaviKanth",77,"Chittapur")
-print(Obj1.get_name(),Obj1.get_age(),Obj1.get_Location()) 
-Obj2=GrandChild_class("ShaShiKanth",23,"Hyderabad")
-print(Obj2.get_name(),Obj2.get_age(),Obj2.get_Location())                   
-obj3=Parent_class("Ravi")
-print(obj3.get_name())
-obj4=Child_class("Ravi",66)
-print(obj4.get_name(),obj4.get_age())
+# class Parent_class:
+#     def __init__(self,name):
+#         self.name=name
+#     def get_name(self):
+#         return self.name
+# class Child_class(Parent_class):
+#     def __init__(self,name,age):
+#         Parent_class.__init__(self,name)
+#         self.age=age
+#     def get_age(self):
+#         return self.age
+# class GrandChild_class(Child_class):
+#     def __init__(self,name,age,location):
+#         Child_class.__init__(self,name,age)
+#         self.location=location
+#     def get_Location(self):
+#         return self.location
+# Obj1=GrandChild_class("RaviKanth",77,"Chittapur")
+# print(Obj1.get_name(),Obj1.get_age(),Obj1.get_Location()) 
+# Obj2=GrandChild_class("ShaShiKanth",23,"Hyderabad")
+# print(Obj2.get_name(),Obj2.get_age(),Obj2.get_Location())                   
+# obj3=Parent_class("Ravi")
+# print(obj3.get_name())
+# obj4=Child_class("Ravi",66)
+# print(obj4.get_name(),obj4.get_age())
+
+class XYZ:
+    def __init__(self):
+        print("Hey.. I have been Initiliazed :",XYZ)
+    def sub_xyz(self,b):
+        print("Printing from class xyz:",b)
+class XYZ1(XYZ):
+    def __init__(self):
+        print(" I am Initiliazed XYZ1") 
+        super().__init__()
+class XYZ2(XYZ1):
+    def __init__(self):
+        print("Hey I am Initialize XYZ2")
+        super().__init__()
+    def sub_xyx2(self,b):
+        print("printing from Class XYZ2")
+        super().__init__()
+if __name__=="__main__":
+    Obj=XYZ2()
+    Obj.sub_xyz(10)        
