f13974e04022b4c837c780836aa257905f3fcb92
ravikanth995 <ravikanthchauhan95@gmail.com>
2023-02-15 12:28:02 +0530

    Find area of Circle and Circumference by input method

---
+++ python_interview.py
@@ -0,0 +1,204 @@
+#Program to display Name, age and Address one by one and display them
+# name=input("Enter Name:")
+# age=int(input("Enter Age :"))
+# Address=input("Enter Address:")
+# print("Name:{}\nAge:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# address=input("Enter Address:")
+# print("Name is :{}\nAge is {}\nAddress is:{}".format(name,age,address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# address=input("Enter Address:")
+# print("Name is:{}\nAge is:{}\nAddress is:{}".format(name,age,address))
+
+# name=input("Enter Name :")
+# age=int(input("Enter Age :"))
+# address=input("Enter Address:")
+# print("Name is:{}\nAge is:{}\nAddress is{}".format(name,age,address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# address=input("Enter Address :")
+# print("Name is:{}\nAge is:{}\nAddress is:{}".format(name,age,address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Address Location:")
+# print("Name is:{}\nAge is:{}\nAddress Location is:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Adddress:")
+# print("Name is :{}\nAge is:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Location=input("Enter Location:")
+# print("Name is:{}\Age is:{}\nLocation is:{}".format(name,age,Location))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age is:"))
+# Address=input("Enter Address:")
+# print("Name is:{}\nAge:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Address:")
+# print("Name is:{}\nAge is:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# address=input("Enter Address:")
+# print("Name is:{}\nAge is:{}\nAddress:{}".format(name,age,address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Address:")
+# print("Name is:{}\nAge:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age :"))
+# Address=input("Enter Address :")
+# print("Name:{}\nAge is:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Address:")
+# print("Name:{}\nAge is:{}\nAddress:{}".format(name,age,Address))
+
+# name=input("Enter Name:")
+# age=int(input("Enter Age:"))
+# Address=input("Enter Address:")
+# print("Name is:{}\nage is:{}\nAddress:{}".format(name,age,Address))
+
+# a=input("Enter Name:")
+# b=int(input("Enter Age :"))
+# c=input("Enter Address:")
+# print("Name:{}\nAge:{}\nAddress:{}".format(a,b,c))
+
+# a=input("Enter Name:")
+# b=int(input("Enter Age:"))
+# c=input("Enter Address:")
+# print("Name:{}\nAge:{}\nAddress:{}".format(a,b,c))
+
+#Find radius of a Circle and Find the Area
+# Pi=3.142
+# radius=float(input("Enter radius of a Circle:"))
+# area=Pi*radius*radius
+# circumf=2*Pi*area
+# print("Area of a circle:%.2f"%area)
+# print("Circumference of a circle:%.3f"%circumf)
+
+# Pi=3.142
+# Area=float(input("Enter Area of Circle:"))
+# circumference=2*Pi*Area
+# print("Area of Circle is:%.3f"%Area)
+# print("Circumference of Circle:%.3f"%circumference)
+
+# Pi=3.142
+# radius=float(input("Enter Radius of a Circle:"))
+# Area=Pi*radius*radius
+# Circumference=2*Pi*Area
+# print("Area of a Circle:%.3f"%Area)
+# print("Circference of a Circle:%.3f"%Circumference)
+
+# Pi=3.142
+# r=float(input("Enter Radius:"))
+# area=Pi*r*r
+# Circumference=2*Pi*area
+# print("Area of Circle:%.3f"%area)
+# print("Circumference of a Circle:%.3f"%Circumference)
+
+# Pi=3.142
+# r=float(input("Enter Radius of a Circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle is:%.3f"%area)
+# print(f"Circumference of a Circle:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("area of Circle:%.3f"%area)
+# print("Circumference of a Circle:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter Radius of a Cirle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle:%.3f"%area)
+# print("Circumference of the Circle:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter Radius:"))
+# area=Pi*r*r
+# circumferece=2*Pi*area
+# print("Area of Circle:%.2f"%area)
+# print("Circumference of a Circle:%.3f"%circumferece)
+
+# Pi=3.142
+# r=float(input("Enter Radius of a Circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle:%.4f"%area)
+# print("Circumference of a circle:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle is:%.3f"%area)
+# print("Circumference of a circle is:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter radius of a circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle:%.3f"%area)
+# print("Circumference of a circle is:%.3f"%circumference)
+
+# Pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=Pi*r*r
+# circumference=2*Pi*area
+# print("Area of Circle:%.3f"%area)
+# print("Circumferemnce of a circle:%.4f"%circumference)
+
+# pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=pi*r*r
+# circumference=2*pi*area
+# print("Area of circle:%.2f"%area)
+# print("circumference of a circle:%.2f"%circumference)
+
+# pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=pi*r*r
+# circumference=2*pi*area
+# print("Area of Circle is:%.3f"%area)
+# print("Circumference of a Circle is:%.3f"%circumference)
+
+# pi=3.142
+# r=float(input("Enter radius of a Circle:"))
+# area=pi*r*r
+# circumference=2*pi*area
+# print("Area of circle is:%.4f"%area)
+# print("Circumference of the Circle:%.4f"%circumference)
+
+# pi=3.142
+# r=float(input("Enter radius of a Cicle:"))
+# area=pi*r*r
+# circumference=2*pi*area
+# print("Area of Circle:%.3f"%area)
+# print("Circumference of a Circle is:%.3f"%circumference)
+
+pi=3.142
+r=float(input("Enter Radius of a circle:"))
+area=pi*r*r
+circumference=2*pi*area
+print("Area of Circle:%.3f"%area)
+print("Circumference of a given Circle is :%.3f"%circumference)
